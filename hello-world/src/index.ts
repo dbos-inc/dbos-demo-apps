3fc0160a0cde11ac9695bd3eb1f8f3631c94b0a3
Peter Kraft <petereliaskraft@gmail.com>
2023-07-13 13:41:33 -0700

    hello world DB

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,35 +1,48 @@
 import express, { Express, Request, Response } from 'express';
 import { FunctionContext, WorkflowContext, Operon, registerFunction, registerWorkflow } from 'operon';
+import * as readline from 'node:readline/promises';
 
 // Register an Operon function
-const helloFunction = registerFunction((functionCtxt: FunctionContext, name: string) => {
+const helloFunction = registerFunction(async (functionCtxt: FunctionContext, name: string) => {
     console.log("hello from function: " + name);
     return "function succeeded!";
 });
 
 // Register an Operon workflow
-const helloWorkflow = registerWorkflow((workflowCtxt: WorkflowContext, name: string) => {
+const helloWorkflow = registerWorkflow(async (workflowCtxt: WorkflowContext, name: string) => {
     console.log("hello from workflow: " + name);
-    const funcResult: string = helloFunction(workflowCtxt, name);
+    const funcResult: string = await helloFunction(workflowCtxt, name);
     console.log("workflow got function output: " + funcResult);
     return "workflow succeeded!";
 });
 
-// Invoke the workflow from an Express HTTP handler
-const app: Express = express();
-const port = 3000;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-const operon: Operon = new Operon();
 
-app.get('/:name', (req: Request, res: Response) => {
-    operon.helloWorld();
-    const { name } = req.params;
-    const workflowResult: string = helloWorkflow(operon, name);
-    console.log("handler got workflow output: " + workflowResult);
-    res.send('Operon says hello to: ' + name);
-});
+async function startServer() {
+    // Initialize Postgres and Operon.
+    const rl = readline.createInterface(process.stdin, process.stdout);
+    const username = await rl.question('Enter postgres username: ');
+    const password = await rl.question('Enter postgres password: ');
+    rl.close();
+    const operon: Operon = new Operon({
+        user: username,
+        password: password,
+    });
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-})
+    // Invoke the workflow from an Express HTTP handler
+    const app: Express = express();
+    const port = 3000;
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.get('/:name', async (req: Request, res: Response) => {
+        const { name } = req.params;
+        const workflowResult: string = await helloWorkflow(operon, name);
+        console.log("handler got workflow output: " + workflowResult);
+        res.send('Operon says hello to: ' + name);
+    });
+    app.listen(port, () => {
+        console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+
+}
+
+startServer();
