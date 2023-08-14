46375c15679b57ad4778d7fea4b81f3c08a9bbd8
Peter Kraft <peter.kraft@dbos.dev>
2023-08-14 10:36:37 -0700

    Update Shop Demo (#9)
    Update Shop and hello-world demos as of
    https://github.com/dbos-inc/operon/pull/27.

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,11 +1,10 @@
 import express, { Express, Request, Response } from 'express';
 import { TransactionContext, WorkflowContext, Operon, } from 'operon';
-import * as readline from 'node:readline/promises';
 
 // Declare an Operon function
 const helloFunction = async (txnCtxt: TransactionContext, name: string) => {
   const greeting = `Hello, ${name}!`
-  const { rows } = await txnCtxt.client.query("INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id", [greeting])
+  const { rows } = await txnCtxt.pgClient.query("INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id", [greeting])
   return `Greeting ${rows[0].greeting_id}: ${greeting}`;
 };
 
@@ -17,8 +16,9 @@ const helloWorkflow = async (workflowCtxt: WorkflowContext, name: string) => {
 async function startServer() {
   // Initialize Postgres and Operon.
   const operon: Operon = new Operon();
+  operon.useNodePostgres();
   await operon.init();
-  await operon.pool.query("CREATE TABLE IF NOT EXISTS OperonHello (greeting_id SERIAL PRIMARY KEY, greeting TEXT);");
+  await operon.userDatabase.query("CREATE TABLE IF NOT EXISTS OperonHello (greeting_id SERIAL PRIMARY KEY, greeting TEXT);");
   operon.registerTransaction(helloFunction);
   operon.registerWorkflow(helloWorkflow);
 
@@ -29,7 +29,7 @@ async function startServer() {
   app.use(express.urlencoded({ extended: true }));
   app.get('/greeting/:name', async (req: Request, res: Response) => {
     const { name } = req.params;
-    const greeting: string = await operon.workflow(helloWorkflow, {}, name);
+    const greeting: string = await operon.workflow(helloWorkflow, {}, name).getResult();
     res.send(greeting);
   });
   app.listen(port, () => {
