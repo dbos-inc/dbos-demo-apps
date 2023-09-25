6700f5016349d6853142ebe86e0cf5b25f29ad3c
Chuck B <chuck.bear@dbos.dev>
2023-09-25 09:46:29 -0400

    Add initial README.md

--- yky-social/scripts/init_pgdb.sh
+++ yky-social/scripts/init_pgdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Create the new user 'shop'
+# Create the new user 'socialts'
 psql -U postgres -h localhost -c "CREATE USER socialts WITH PASSWORD 'socialts';"
 
 # Drop the DB if it already exists, then create it again
