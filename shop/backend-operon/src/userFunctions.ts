36aa42e9a1cebb980f211f751bccce58a3fd57f1
Peter Kraft <petereliaskraft@gmail.com>
2023-09-26 13:15:41 -0700

    cleaning up

--- shop/backend-operon/src/userFunctions.ts
+++ shop/backend-operon/src/userFunctions.ts
@@ -1,6 +1,6 @@
 import {
     TransactionContext, WorkflowContext, OperonTransaction, OperonWorkflow, HandlerContext,
-    GetApi, PostApi, OperonCommunicator, CommunicatorContext, OperonResponseError
+    GetApi, PostApi, OperonCommunicator, CommunicatorContext, OperonResponseError, ArgSource, ArgSources
 } from '@dbos-inc/operon';
 import Stripe from 'stripe';
 
@@ -28,31 +28,8 @@ const checkout_complete_topic = "stripe_checkout_complete";
 
 export class Shop {
 
-    @PostApi('/stripe_webhook')
-    static async stripeWebhook(ctxt: HandlerContext) {
-        const req = ctxt.koaContext.request;
-        const sigHeader = req.headers['stripe-signature'];
-        if (typeof sigHeader !== 'string') {
-            throw new OperonResponseError("Invalid Header", 400);
-        }
-        const payload: string = req.rawBody;
-        try {
-            const event = stripe.webhooks.constructEvent(payload, sigHeader, endpointSecret);
-            if (event.type === 'checkout.session.completed') {
-                const session = await stripe.checkout.sessions.retrieve((event.data.object as Stripe.Response<Stripe.Checkout.Session>).id);
-                if (session.client_reference_id !== null) {
-                    const uuid: string = session.client_reference_id;
-                    await ctxt.operon.send({}, uuid, "checkout.session.completed", checkout_complete_topic);
-                }
-            }
-        } catch (err) {
-            console.log(err);
-            throw new OperonResponseError("Webhook Error", 400);
-        }
-    }
-
-    @OperonTransaction()
     @GetApi('/api/products')
+    @OperonTransaction()
     static async getProducts(txnCtxt: TransactionContext): Promise<Product[]> {
         const { rows } = await txnCtxt.pgClient.query<Product>('SELECT product_id, product, description, image_name, price FROM products');
         const formattedRows: Product[] = rows.map((row) => ({
@@ -66,7 +43,6 @@ export class Shop {
     @GetApi('/api/products/:id')
     @OperonTransaction({ readOnly: true })
     static async getProduct(ctxt: TransactionContext, id: number): Promise<Product | null> {
-
         const { rows } = await ctxt.pgClient.query<Product>(`SELECT product_id, product, description, image_name, price FROM products WHERE product_id = $1`, [id]);
         if (rows.length === 0) {
             return null;
@@ -95,64 +71,24 @@ export class Shop {
         return productDetails;
     }
 
-    @OperonTransaction()
-    static async clearCart(ctxt: TransactionContext, username: string) {
-        await ctxt.pgClient.query(`DELETE FROM cart WHERE username=$1`, [username]);
-    }
-
-    @OperonTransaction()
-    static async createOrder(ctxt: TransactionContext, username: string, productDetails: Product[]): Promise<number> {
-        const { rows } = await ctxt.pgClient.query<{ order_id: number }>(`INSERT INTO orders(username, order_status, last_update_time) VALUES ($1, $2, $3) RETURNING order_id`,
-            [username, OrderStatus.PENDING, 0]);
-        const orderID: number = rows[0].order_id;
-        for (const product of productDetails) {
-            await ctxt.pgClient.query(`INSERT INTO order_items(order_id, product_id, price, quantity) VALUES($1, $2, $3, $4)`,
-                [orderID, product.product_id, product.price, product.inventory]);
+    @PostApi('/api/checkout_session')
+    static async webCheckout(ctxt: HandlerContext, @ArgSource(ArgSources.QUERY) username: string) {
+        const origin = ctxt.request?.headers.origin as string;
+        if (typeof username !== 'string' || typeof origin !== 'string') {
+            throw new OperonResponseError("Invalid request!", 400);
         }
-        return orderID;
-    }
+        const handle = ctxt.operon.workflow(Shop.paymentWorkflow, {}, username, origin);
+        const url = await ctxt.operon.getEvent<string>(handle.getWorkflowUUID(), checkout_url_topic);
 
-    @OperonTransaction()
-    static async undoSubtractInventory(ctxt: TransactionContext, products: Product[]) {
-        for (const product of products) {
-            await ctxt.pgClient.query(`UPDATE products SET inventory = inventory + $1 WHERE product_id = $2`, [product.inventory, product.product_id]);
+        if (url === null) {
+            ctxt.koaContext.redirect(`${origin}/checkout/cancel`);
+        } else {
+            ctxt.koaContext.redirect(url);
         }
-    }
-
-    @OperonTransaction()
-    static async subtractInventory(ctxt: TransactionContext, products: Product[]): Promise<boolean> {
-        let hasEnoughInventory = true;
-        for (const product of products) {
-            const { rows } = await ctxt.pgClient.query<Product>(`SELECT inventory FROM products WHERE product_id = $1`, [product.product_id]);
-            const currentInventory = rows[0]?.inventory;
-
-            if (currentInventory < product.inventory) {
-                hasEnoughInventory = false;
-                break;
-            }
-        }
-        if (hasEnoughInventory) {
-            // If all products have enough inventory, subtract the inventory from the products in the database
-            for (const product of products) {
-                await ctxt.pgClient.query(`UPDATE products SET inventory = inventory - $1 WHERE product_id = $2`, [product.inventory, product.product_id]);
-            }
-        }
-        return hasEnoughInventory;
-    }
-
-    @OperonTransaction()
-    static async fulfillOrder(ctxt: TransactionContext, orderID: number) {
-        await ctxt.pgClient.query(`UPDATE orders SET order_status=$1 WHERE order_id=$2`, [OrderStatus.FULFILLED, orderID]);
-    }
-
-    @OperonTransaction()
-    static async errorOrder(ctxt: TransactionContext, orderID: number) {
-        await ctxt.pgClient.query(`UPDATE orders SET order_status=$1 WHERE order_id=$2`, [OrderStatus.CANCELLED, orderID]);
     }
 
     @OperonWorkflow()
     static async paymentWorkflow(ctxt: WorkflowContext, username: string, origin: string) {
-
         const productDetails = await ctxt.transaction(Shop.getCart, username);
         if (productDetails.length === 0) {
             await ctxt.setEvent(checkout_url_topic, null);
@@ -202,25 +138,59 @@ export class Shop {
         }
     }
 
-    @PostApi('/api/checkout_session')
-    static async webCheckout(ctxt: HandlerContext) {
-
-        const req = ctxt.koaContext.request;
-
-        const username = req.query.username as string;
-        const origin = req.headers.origin as string;
-
-        if (typeof username !== 'string' || typeof origin !== 'string') {
-            throw new OperonResponseError("Invalid request!", 400);
+    @OperonTransaction()
+    static async createOrder(ctxt: TransactionContext, username: string, productDetails: Product[]): Promise<number> {
+        const { rows } = await ctxt.pgClient.query<{ order_id: number }>(`INSERT INTO orders(username, order_status, last_update_time) VALUES ($1, $2, $3) RETURNING order_id`,
+            [username, OrderStatus.PENDING, 0]);
+        const orderID: number = rows[0].order_id;
+        for (const product of productDetails) {
+            await ctxt.pgClient.query(`INSERT INTO order_items(order_id, product_id, price, quantity) VALUES($1, $2, $3, $4)`,
+                [orderID, product.product_id, product.price, product.inventory]);
         }
-        const handle = ctxt.operon.workflow(Shop.paymentWorkflow, {}, username, origin);
-        const url = await ctxt.operon.getEvent<string>(handle.getWorkflowUUID(), checkout_url_topic);
+        return orderID;
+    }
 
-        if (url === null) {
-            ctxt.koaContext.redirect(`${origin}/checkout/cancel`);
-        } else {
-            ctxt.koaContext.redirect(url);
+    @OperonTransaction()
+    static async subtractInventory(ctxt: TransactionContext, products: Product[]): Promise<boolean> {
+        let hasEnoughInventory = true;
+        for (const product of products) {
+            const { rows } = await ctxt.pgClient.query<Product>(`SELECT inventory FROM products WHERE product_id = $1`, [product.product_id]);
+            const currentInventory = rows[0]?.inventory;
+
+            if (currentInventory < product.inventory) {
+                hasEnoughInventory = false;
+                break;
+            }
         }
+        if (hasEnoughInventory) {
+            // If all products have enough inventory, subtract the inventory from the products in the database
+            for (const product of products) {
+                await ctxt.pgClient.query(`UPDATE products SET inventory = inventory - $1 WHERE product_id = $2`, [product.inventory, product.product_id]);
+            }
+        }
+        return hasEnoughInventory;
+    }
+
+    @OperonTransaction()
+    static async undoSubtractInventory(ctxt: TransactionContext, products: Product[]) {
+        for (const product of products) {
+            await ctxt.pgClient.query(`UPDATE products SET inventory = inventory + $1 WHERE product_id = $2`, [product.inventory, product.product_id]);
+        }
+    }
+
+    @OperonTransaction()
+    static async fulfillOrder(ctxt: TransactionContext, orderID: number) {
+        await ctxt.pgClient.query(`UPDATE orders SET order_status=$1 WHERE order_id=$2`, [OrderStatus.FULFILLED, orderID]);
+    }
+
+    @OperonTransaction()
+    static async errorOrder(ctxt: TransactionContext, orderID: number) {
+        await ctxt.pgClient.query(`UPDATE orders SET order_status=$1 WHERE order_id=$2`, [OrderStatus.CANCELLED, orderID]);
+    }
+
+    @OperonTransaction()
+    static async clearCart(ctxt: TransactionContext, username: string) {
+        await ctxt.pgClient.query(`DELETE FROM cart WHERE username=$1`, [username]);
     }
 
     @OperonCommunicator()
@@ -253,5 +223,28 @@ export class Shop {
             // Session was already expired.
         }
         return session;
+    }
+
+    @PostApi('/stripe_webhook')
+    static async stripeWebhook(ctxt: HandlerContext) {
+        const req = ctxt.koaContext.request;
+        const sigHeader = req.headers['stripe-signature'];
+        if (typeof sigHeader !== 'string') {
+            throw new OperonResponseError("Invalid Header", 400);
+        }
+        const payload: string = req.rawBody;
+        try {
+            const event = stripe.webhooks.constructEvent(payload, sigHeader, endpointSecret);
+            if (event.type === 'checkout.session.completed') {
+                const session = await stripe.checkout.sessions.retrieve((event.data.object as Stripe.Response<Stripe.Checkout.Session>).id);
+                if (session.client_reference_id !== null) {
+                    const uuid: string = session.client_reference_id;
+                    await ctxt.operon.send({}, uuid, "checkout.session.completed", checkout_complete_topic);
+                }
+            }
+        } catch (err) {
+            console.log(err);
+            throw new OperonResponseError("Webhook Error", 400);
+        }
     }
 }
