37f3c57d22b04b1669ef1b72fcbcce673a104b78
Qian Li <qian.li@dbos.dev>
2023-09-28 12:31:51 -0700

    Bank use middleware decorators (#46)
    This PR updates bank to use the latest middleware decorators in Operon
    v0.0.4.
    
    In general it works smoothly, but we need to be very careful about
    circular dependencies as it can cause the middleware decorator input to
    be `undefined`. That's the reason we have to declare bank middleware
    functions in a separate file here.

--- bank/bank-backend/src/router.ts
+++ bank/bank-backend/src/router.ts
@@ -1,30 +1,64 @@
 import { bankname } from "./main";
 import { TransactionHistory } from "@prisma/client";
 import { BankTransactionHistory } from "./workflows/txnhistory.workflows";
-import { MiddlewareContext, OperonResponseError, RequiredRole, GetApi, HandlerContext, PostApi } from "@dbos-inc/operon";
+import { OperonResponseError, GetApi, HandlerContext, PostApi, DefaultRequiredRole, Authentication, KoaMiddleware } from "@dbos-inc/operon";
+import { bankAuthMiddleware, koaLogger, customizeHandle, bankJwt } from "./middleware";
 
-// eslint-disable-next-line @typescript-eslint/require-await
-export async function bankAuthMiddleware(ctx: MiddlewareContext) {
-  if (ctx.requiredRole.length > 0) {
-    console.log("required role: ", ctx.requiredRole);
-    if (!ctx.koaContext) {
-      throw new OperonResponseError("No Koa context!");
-    } else if (!ctx.koaContext.state.user) {
-      throw new OperonResponseError("No authenticated user!", 401);
+@DefaultRequiredRole(["appUser"])
+@Authentication(bankAuthMiddleware)
+@KoaMiddleware(koaLogger, customizeHandle, bankJwt)
+export class BankEndpoints {
+  // Can we have some class-wide default required roles?
+  // eslint-disable-next-line @typescript-eslint/require-await
+  @GetApi("/api/greeting")
+  static async greeting(ctx: HandlerContext) {
+    void ctx;
+    return { msg: `Hello from DBOS Operon ${bankname}!` };
+  }
+
+  // Deposit.
+  @PostApi("/api/deposit")
+  static async deposit(ctx: HandlerContext) {
+    const data = convertTransactionHistory(ctx.koaContext.request.body as TransactionHistory);
+    if (!data.fromLocation) {
+      throw new OperonResponseError("fromLocation must not be empty!", 400);
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-    const authenticatedUser: string = ctx.koaContext.state.user["preferred_username"] ?? "";
-    console.log("current user: ", authenticatedUser);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-    const authenticatedRoles: string[] = ctx.koaContext.state.user["realm_access"]["roles"] ?? [];
-    console.log("JWT claimed roles: ", authenticatedRoles);
-    if (authenticatedRoles.includes("appAdmin")) {
-      // appAdmin role has more priviledges than appUser.
-      authenticatedRoles.push("appUser");
+    // Must to local.
+    data.toLocation = "local";
+
+    return ctx.workflow(BankTransactionHistory.depositWorkflow, {}, data).getResult();
+  }
+
+  // Withdraw.
+  @PostApi("/api/withdraw")
+  static async withdraw(ctx: HandlerContext) {
+    const data = convertTransactionHistory(ctx.koaContext.request.body as TransactionHistory);
+    if (!data.toLocation) {
+      throw new OperonResponseError("toLocation must not be empty!", 400);
     }
-    console.log("authenticated roles: ", authenticatedRoles);
-    return { authenticatedUser: authenticatedUser, authenticatedRoles: authenticatedRoles };
+
+    // Must from local.
+    data.fromLocation = "local";
+
+    return ctx.workflow(BankTransactionHistory.withdrawWorkflow, {}, data).getResult();
+  }
+
+  // Internal transfer
+  @PostApi("/api/transfer")
+  static async internalTransfer(ctx: HandlerContext) {
+    const data = convertTransactionHistory(ctx.koaContext.request.body as TransactionHistory);
+    // Check the transaction is within the local database.
+    if ((data.fromLocation !== undefined && data.fromLocation !== "local") || (data.toLocation !== undefined && data.toLocation !== "local")) {
+      throw new Error("Must be a local transaction! Instead: " + data.fromLocation + " -> " + data.toLocation);
+    }
+
+    // Check valid input.
+    if (!data.toLocation || !data.toAccountId || !data.amount || !data.fromAccountId || !data.fromLocation) {
+      throw new Error("Invalid input!");
+    }
+
+    return ctx.transaction(BankTransactionHistory.internalTransferFunc, {}, data);
   }
 }
 
@@ -43,64 +77,4 @@ function convertTransactionHistory(data: TransactionHistory): TransactionHistory
     amount: data.amount,
     timestamp: data.timestamp ?? undefined,
   };
-}
-
-export class BankEndpoints {
-
-  // Can we have some class-wide default required roles?
-  // eslint-disable-next-line @typescript-eslint/require-await
-  @GetApi("/api/greeting")
-  @RequiredRole(['appUser'])
-  static async greeting(ctx: HandlerContext) {
-    void ctx;
-    return { msg: `Hello from DBOS Operon ${bankname}!` };
-  }
-
-  // Deposit.
-  @PostApi("/api/deposit")
-  @RequiredRole(['appUser'])
-  static async deposit(ctx: HandlerContext) {
-    const data = convertTransactionHistory(ctx.koaContext.request.body as TransactionHistory);
-    if (!data.fromLocation) {
-      throw new OperonResponseError("fromLocation must not be empty!", 400);
-    }
-
-    // Must to local.
-    data.toLocation = "local";
-
-    return ctx.workflow(BankTransactionHistory.depositWorkflow, {}, data).getResult();
-  }
-
-  // Withdraw.
-  @PostApi("/api/withdraw")
-  @RequiredRole(['appUser'])
-  static async withdraw(ctx: HandlerContext) {
-    const data = convertTransactionHistory(ctx.koaContext.request.body as TransactionHistory);
-    if (!data.toLocation) {
-      throw new OperonResponseError("toLocation must not be empty!", 400);
-    }
-
-    // Must from local.
-    data.fromLocation = "local";
-
-    return ctx.workflow(BankTransactionHistory.withdrawWorkflow, {}, data).getResult();
-  }
-
-  // Internal transfer
-  @PostApi("/api/transfer")
-  @RequiredRole(['appUser'])
-  static async internalTransfer(ctx: HandlerContext) {
-    const data = convertTransactionHistory(ctx.koaContext.request.body as TransactionHistory);
-    // Check the transaction is within the local database.
-    if ((data.fromLocation !== undefined && data.fromLocation !== "local") || (data.toLocation !== undefined && data.toLocation !== "local")) {
-      throw new Error("Must be a local transaction! Instead: " + data.fromLocation + " -> " + data.toLocation);
-    }
-
-    // Check valid input.
-    if (!data.toLocation || !data.toAccountId || !data.amount || !data.fromAccountId || !data.fromLocation) {
-      throw new Error("Invalid input!");
-    }
-
-    return ctx.transaction(BankTransactionHistory.internalTransferFunc, {}, data);
-  }
 }
