af971aa6c7ee4f76d55130e878817dc6bc244e40
Chuck B <chuck.bear@dbos.dev>
2023-09-28 10:22:26 -0400

    Use communicator

--- yky-social/src/app.ts
+++ yky-social/src/app.ts
@@ -29,8 +29,7 @@ import {
 } from "@dbos-inc/operon";
 
 
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { S3Client } from '@aws-sdk/client-s3';
 
 const s3ClientConfig = {
   region: process.env.AWS_REGION || 'us-east-2', // Replace with your AWS region
