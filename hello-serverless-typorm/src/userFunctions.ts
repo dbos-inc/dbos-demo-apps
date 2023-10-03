325e65647867052ab2b4fa026b850a810da1200e
manojdbos <manoj.khangaonkar@dbos.dev>
2023-10-03 11:49:04 -0700

    clean up

--- hello-serverless-typorm/src/userFunctions.ts
+++ hello-serverless-typorm/src/userFunctions.ts
@@ -1,6 +1,5 @@
 import { TransactionContext, WorkflowContext, OperonTransaction, OperonWorkflow, GetApi, OrmEntities } from 'operon';
 import { Entity, EntityManager, PrimaryGeneratedColumn, Column } from "typeorm"
-// import { OperonHello } from './OperonHello'
 
 @Entity()
 export class OperonHello {
@@ -9,7 +8,6 @@ export class OperonHello {
 
     @Column()
     greeting: string = "greeting"
-
 } 
 
 @OrmEntities([OperonHello])
@@ -22,51 +20,19 @@ export class Hello {
     const p: EntityManager = txnCtxt.typeormEM as EntityManager ;
     const g: OperonHello = new OperonHello();
     g.greeting = greeting;
-    console.log("In hello function " + __dirname);
     const res = await p.save(g);
-
-    console.log(res);
-
-    // const { rows } = await txnCtxt.pgClient.query("INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id", [greeting])
     return `Greeting ${res.greeting_id}: ${greeting}`;
   };
 
   @OperonWorkflow()
   @GetApi('/greeting/:name')
   static async helloWorkflow(workflowCtxt: WorkflowContext, name: string) {
-
-    console.log("received http request " +name)
-
-    if (check(OperonHello, "EntitySchema")) {
-      console.log("Valid entity");
-    } else {
-      console.log("Not Valid entity");
-    }
-
     return await workflowCtxt.transaction(Hello.helloFunction, name);
   };
 
 }
 
-function check(obj: unknown, name: string) {
 
-  if (typeof obj === "object") {
-    console.log("It is an object");
-  } else {
-    console.log("It is not an object");
-  }
-  console.log(Symbol.for(name));
-  const v = obj as { "@instanceof": Symbol };
-  console.log(v);
-  console.log(v["@instanceof"]);
-
-  return (
-      typeof obj === "object" &&
-      obj !== null &&
-      (obj as { "@instanceof": Symbol })["@instanceof"] ===
-          Symbol.for(name)
-  )
-}
 
 
 
