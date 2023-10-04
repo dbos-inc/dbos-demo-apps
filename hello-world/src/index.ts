8bf2d90f42f450e740f079b7961be1da28188722
maxdml <maxdml@dbos.dev>
2023-10-03 17:04:27 -0700

    serverless hello world

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,40 +1,36 @@
-import express, { Express, Request, Response } from 'express';
-import { TransactionContext, WorkflowContext, Operon, } from '@dbos-inc/operon';
+import {
+  TransactionContext,
+  WorkflowContext,
+  OperonTransaction,
+  OperonWorkflow,
+  GetApi,
+  HandlerContext,
+} from "@dbos-inc/operon";
 
-// Declare an Operon function
-const helloFunction = async (txnCtxt: TransactionContext, name: string) => {
-  const greeting = `Hello, ${name}!`
-  const { rows } = await txnCtxt.pgClient.query("INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id", [greeting])
-  return `Greeting ${rows[0].greeting_id}: ${greeting}`;
-};
+export class Hello {
+  @OperonTransaction()
+  static async helloFunction(txnCtxt: TransactionContext, name: string) {
+    const greeting = `Hello, ${name}!`;
+    const { rows } = await txnCtxt.pgClient.query<{ greeting_id: number }>(
+      "INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id",
+      [greeting]
+    );
+    txnCtxt.log(`Inserted greeting ${rows[0].greeting_id}: ${greeting}`);
+    return `Greeting ${rows[0].greeting_id}: ${greeting}`;
+  }
 
-// Declare an Operon workflow
-const helloWorkflow = async (workflowCtxt: WorkflowContext, name: string) => {
-  return await workflowCtxt.transaction(helloFunction, name);
-};
+  @OperonWorkflow()
+  static async helloWorkflow(wfCtxt: WorkflowContext, name: string) {
+    wfCtxt.log("Hello, workflow!");
+    return await wfCtxt.invoke(Hello).helloFunction(name);
+  }
 
-async function startServer() {
-  // Initialize Postgres and Operon.
-  const operon: Operon = new Operon();
-  operon.useNodePostgres();
-  await operon.init();
-  await operon.userDatabase.query("CREATE TABLE IF NOT EXISTS OperonHello (greeting_id SERIAL PRIMARY KEY, greeting TEXT);");
-  operon.registerTransaction(helloFunction);
-  operon.registerWorkflow(helloWorkflow);
-
-  // Invoke the workflow from an Express HTTP handler
-  const app: Express = express();
-  const port = 3000;
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.get('/greeting/:name', async (req: Request, res: Response) => {
-    const { name } = req.params;
-    const greeting: string = await operon.workflow(helloWorkflow, {}, name).getResult();
-    res.send(greeting);
-  });
-  app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-  });
+  @GetApi("/greeting/:name")
+  static async helloEndpoint(ctx: HandlerContext, name: string) {
+    ctx.log("helloEndpoint");
+    return await ctx
+      .invoke(Hello)
+      .helloWorkflow(name)
+      .then((x) => x.getResult());
+  }
 }
-
-startServer();
