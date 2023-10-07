29fe5b8b1bdb3e8965d566a4909c190f6d7b0d9a
Peter Kraft <petereliaskraft@gmail.com>
2023-10-06 17:32:25 -0700

    simple communicator

--- operon-hello-extended/src/userFunctions.ts
+++ operon-hello-extended/src/userFunctions.ts
@@ -1,5 +1,6 @@
-import { TransactionContext, OperonTransaction, GetApi, HandlerContext, PostApi } from '@dbos-inc/operon'
+import { TransactionContext, OperonTransaction, GetApi, HandlerContext, PostApi, CommunicatorContext, OperonCommunicator } from '@dbos-inc/operon'
 import { Knex } from 'knex';
+import axios from 'axios';
 
 type KnexTransactionContext = TransactionContext<Knex>;
 
@@ -13,13 +14,13 @@ export class Hello {
   static async helloTransaction(txnCtxt: KnexTransactionContext, name: string) {
     // Increment greet_count.
     await txnCtxt.client<operon_hello>("operon_hello")
-      .insert({name: name, greet_count: 1})
+      .insert({ name: name, greet_count: 1 })
       .onConflict('name')
       .merge({ greet_count: txnCtxt.client.raw('operon_hello.greet_count + 1') });
     // Retrieve greet_count.
     const greet_count = await txnCtxt.client<operon_hello>("operon_hello")
       .select("greet_count")
-      .where({name:name})
+      .where({ name: name })
       .first()
       .then(row => row?.greet_count);
     return `Hello, ${name}! You have been greeted ${greet_count} times.\n`;
@@ -27,14 +28,16 @@ export class Hello {
 
   @GetApi('/greeting/:name')
   static async helloHandler(handlerCtxt: HandlerContext, name: string) {
-    return handlerCtxt.invoke(Hello).helloTransaction(name);
+    const greeting = await handlerCtxt.invoke(Hello).helloTransaction(name);
+    handlerCtxt.invoke(Hello).postmanFunction(greeting);
+    return greeting;
   }
 
   @OperonTransaction()
   static async clearTransaction(txnCtxt: KnexTransactionContext, name: string) {
-    // Delete a users's entry.
+    // Delete greet_count for a user.
     await txnCtxt.client<operon_hello>("operon_hello")
-      .where({name: name})
+      .where({ name: name })
       .delete()
     return `Cleared greet_count for ${name}!\n`
   }
@@ -42,5 +45,21 @@ export class Hello {
   @PostApi('/clear/:name')
   static async clearHandler(handlerCtxt: HandlerContext, name: string) {
     return handlerCtxt.invoke(Hello).clearTransaction(name);
+  }
+
+  @OperonCommunicator()
+  static async postmanFunction(_commCtxt: CommunicatorContext, greeting: string) {
+    try {
+      await axios.get("https://postman-echo.com/get", {
+        params: {
+          greeting: greeting
+        }
+      });
+      console.log(`Greeting sent to postman!`)
+      return true;
+    } catch (e) {
+      console.warn("Error sending request:", e);
+      return false;
+    }
   }
 }
