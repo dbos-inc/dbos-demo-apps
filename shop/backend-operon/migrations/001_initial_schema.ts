66945cf7ce9545030ab6317aad0be72a987e6b9c
Harry Pierson <harry.pierson@dbos.dev>
2023-10-08 14:46:36 -0700

    add initial knex files (knexfile, migration + seed)

---
+++ shop/backend-operon/migrations/001_initial_schema.ts
@@ -0,0 +1,49 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('cart', table => {
+        table.string('username', 255).notNullable();
+        table.integer('product_id').notNullable();
+        table.integer('quantity').notNullable().defaultTo(1);
+        table.primary(['username', 'product_id']);
+    });
+
+    await knex.schema.createTable('users', table => {
+        table.string('username', 255).primary();
+        table.string('password', 255).notNullable();
+    });
+
+    await knex.schema.createTable('orders', table => {
+        table.increments('order_id').primary();
+        table.string('username', 255).notNullable();
+        table.integer('order_status').notNullable();
+        table.string('stripe_session_id', 255).notNullable().defaultTo("");
+        table.bigInteger('last_update_time').notNullable();
+    });
+
+    await knex.schema.createTable('order_items', table => {
+        table.integer("order_id").notNullable();
+        table.integer("product_id").notNullable();
+        table.integer("price").notNullable();
+        table.integer("quantity").notNullable();
+        table.primary(['order_id', 'product_id']);
+        table.foreign('order_id').references('orders.order_id');
+    });
+
+    await knex.schema.createTable('products', table => {
+        table.increments('product_id').primary();
+        table.string('product', 255).unique().notNullable();
+        table.text('description').notNullable();
+        table.string('image_name', 255).notNullable();
+        table.integer('price').notNullable();
+        table.integer('inventory').notNullable();
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('cart');
+    await knex.schema.dropTable('users');
+    await knex.schema.dropTable('orders');
+    await knex.schema.dropTable('order_items');
+    await knex.schema.dropTable('products');
+}
---
+++ shop/backend-operon/seeds/initial.ts
@@ -0,0 +1,24 @@
+import { Knex } from "knex";
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex("products").del();
+
+    // Inserts seed entries
+    await knex("products").insert([
+        { 
+            product: "Pen",
+            description: "This is a great pen.",
+            image_name: "pen.jpg",
+            price: 9999,
+            inventory: 100000
+        },
+        { 
+            product: "Pencil",
+            description: "This is a great pencil.",
+            image_name: "pencil.jpg",
+            price: 8999,
+            inventory: 100000
+        },
+    ]);
+};
