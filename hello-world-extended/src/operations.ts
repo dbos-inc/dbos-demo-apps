0d52bd13021b57c1cca4d95a045893fcb055801a
Peter Kraft <petereliaskraft@gmail.com>
2023-10-12 15:03:41 -0700

    reformat

--- hello-world-extended/src/operations.ts
+++ hello-world-extended/src/operations.ts
@@ -2,8 +2,6 @@ import { TransactionContext, OperonTransaction, GetApi, HandlerContext, PostApi,
 import { Knex } from 'knex';
 import axios from 'axios';
 
-type KnexTransactionContext = TransactionContext<Knex>;
-
 interface operon_hello {
   name: string;
   greet_count: number;
@@ -11,53 +9,12 @@ interface operon_hello {
 
 export class Hello {
 
-  @OperonTransaction()
-  static async helloTransaction(txnCtxt: KnexTransactionContext, name: string) {
-    // Look up greet_count.
-    let greet_count = await txnCtxt.client<operon_hello>("operon_hello")
-      .select("greet_count")
-      .where({ name: name })
-      .first()
-      .then(row => row?.greet_count);
-    if (greet_count) {
-      // If greet_count is set, increment it.
-      greet_count++;
-      await txnCtxt.client<operon_hello>("operon_hello")
-        .where({ name: name })
-        .increment('greet_count', 1);
-    } else {
-      // If greet_count is not set, set it to 1.
-      greet_count = 1;
-      await txnCtxt.client<operon_hello>("operon_hello")
-        .insert({ name: name, greet_count: 1 })
-    }
-    return `Hello, ${name}! You have been greeted ${greet_count} times.\n`;
-  }
-
-  @OperonTransaction()
-  static async rollbackHelloTransaction(txnCtxt: KnexTransactionContext, name: string) {
-    // Decrement greet_count.
-    await txnCtxt.client<operon_hello>("operon_hello")
-      .where({ name: name })
-      .decrement('greet_count', 1);
-  }
-
-  @OperonCommunicator()
-  static async postmanFunction(_commCtxt: CommunicatorContext, greeting: string) {
-    await axios.get("https://postman-echo.com/get", {
-      params: {
-        greeting: greeting
-      }
-    });
-    console.log(`Greeting sent to postman!`);
-  }
-
   @GetApi('/greeting/:name')
   @OperonWorkflow()
   static async helloWorkflow(wfCtxt: WorkflowContext, name: string) {
     const greeting = await wfCtxt.invoke(Hello).helloTransaction(name);
     try {
-      await wfCtxt.invoke(Hello).postmanFunction(greeting);
+      await wfCtxt.invoke(Hello).greetPostman(greeting);
       return greeting;
     } catch (e) {
       console.warn("Error sending request:", e);
@@ -66,13 +23,45 @@ export class Hello {
     }
   }
 
+  @OperonTransaction()  // Declare this function to be a transaction.
+  static async helloTransaction(txnCtxt: TransactionContext<Knex>, user: string) {
+    // Retrieve and increment the number of times this user has been greeted.
+    const rows = await txnCtxt.client<operon_hello>("operon_hello")
+      // Insert greet_count for this user.
+      .insert({ name: user, greet_count: 1 })
+      // If already present, increment it instead.
+      .onConflict("name").merge({ greet_count: txnCtxt.client.raw('operon_hello.greet_count + 1') })
+      // Return the inserted or incremented value.
+      .returning("greet_count");
+    const greet_count = rows[0].greet_count;
+    return `Hello, ${user}! You have been greeted ${greet_count} times.\n`;
+  }
+
+  @OperonTransaction()
+  static async rollbackHelloTransaction(txnCtxt: TransactionContext<Knex>, user: string) {
+    // Decrement greet_count.
+    await txnCtxt.client<operon_hello>("operon_hello")
+      .where({ name: user })
+      .decrement('greet_count', 1);
+  }
+
+  @OperonCommunicator()
+  static async greetPostman(_commCtxt: CommunicatorContext, greeting: string) {
+    await axios.get("https://postman-echo.com/get", {
+      params: {
+        greeting: greeting
+      }
+    });
+    console.log(`Greeting sent to postman!`);
+  }
+
   @PostApi('/clear/:name')
   @OperonTransaction()
-  static async clearTransaction(txnCtxt: KnexTransactionContext, name: string) {
+  static async clearTransaction(txnCtxt: TransactionContext<Knex>, user: string) {
     // Delete greet_count for a user.
     await txnCtxt.client<operon_hello>("operon_hello")
-      .where({ name: name })
+      .where({ name: user })
       .delete()
-    return `Cleared greet_count for ${name}!\n`
+    return `Cleared greet_count for ${user}!\n`
   }
 }
