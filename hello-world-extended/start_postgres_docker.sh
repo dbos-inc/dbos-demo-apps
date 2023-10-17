66a3efe40d6a17d27fed3396c674c7d9b9db9185
Qian Li <qian.li@dbos.dev>
2023-10-16 17:15:06 -0700

    fix postgres start

--- hello-world-extended/start_postgres_docker.sh
+++ hello-world-extended/start_postgres_docker.sh
@@ -12,7 +12,7 @@ docker run --rm --name=operon-db --env=POSTGRES_PASSWORD=${PGPASSWORD} --env=PGD
 # Wait for PostgreSQL to start
 echo "Waiting for PostgreSQL to start..."
 for i in {1..30}; do
-  if docker exec operon-db pg_isready -U postgres > /dev/null; then
+  if docker exec operon-db pg_isready -U postgres | grep -q "accepting connections"; then
     echo "PostgreSQL started!"
     break
   fi
