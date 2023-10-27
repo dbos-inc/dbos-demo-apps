3223034f413498909f1a0a89f4760867e0f14d9b
Harry Pierson <harry.pierson@dbos.dev>
2023-10-27 11:39:02 -0700

    update e-commerce backend dependencies to latest operon preview

--- e-commerce/payment-backend/create-openapi-client.sh
+++ e-commerce/payment-backend/create-openapi-client.sh
@@ -1,9 +1,14 @@
+# generate the openapi definition document
+npx operon openapi src/operations.ts
+
+# generate the typescript-node client
 docker run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli generate \
   -i /local/src/openapi.yaml \
   -g typescript-node \
   -o /local/client
 
+# move the generated client to the frontend
 sudo chown -R $USER:$USER client
 rm -rf ../payment-frontend/client
 mv ./client ../payment-frontend
