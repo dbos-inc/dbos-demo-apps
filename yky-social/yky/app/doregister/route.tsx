0be486fde7e8015f86d2d1245f13821d853d4fd7
Chuck B <chuck.bear@dbos.dev>
2023-10-30 18:43:57 -0400

    Route

--- yky-social/yky/app/doregister/route.tsx
+++ yky-social/yky/app/doregister/route.tsx
@@ -13,8 +13,6 @@ export const config = {
 */
 
 export async function POST(request: Request) {
-  // TODO:  1. What if it throws?  I assume I get an error coming to the client, we'll see
-  // TODO:  2. Can I use the type of the request / return in the front end?
   try {
     return NextResponse.json(await api.doRegister({doRegisterRequest: await request.json()}));
   }
