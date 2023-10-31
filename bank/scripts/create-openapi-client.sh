6ccbba1599cf1163628e531feda62b33d9ec2470
Harry Pierson <harry.pierson@dbos.dev>
2023-10-31 16:40:35 -0700

    openapi

---
+++ bank/scripts/create-openapi-client.sh
@@ -0,0 +1,20 @@
+bank_root=$(dirname $0)/..
+cur_dir=$(pwd)
+
+# generate the openapi definition document
+cd $bank_root/bank-backend
+npx operon openapi src/operations.ts
+mv -f src/openapi.yaml ../bank-frontend
+
+cd ../bank-frontend
+
+# generate the typescript-fetch client
+docker run --rm \
+  -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+  -i /local/openapi.yaml \
+  -g typescript-fetch \
+  -o /local/client
+
+sudo chown -R $USER:$USER client
+
+cd $cur_dir
--- bank/scripts/start_keycloak_docker.sh
+++ bank/scripts/start_keycloak_docker.sh
@@ -15,8 +15,9 @@ POSTGRES_PORT=5432
 docker pull quay.io/keycloak/keycloak:latest
 
 # This script will start a Keycloak server in a docker container with two pre-configured users in the DBOS realm.
-docker run --rm -d -p 8083:8080 --name keycloak -e KEYCLOAK_ADMIN=dbos-admin -e KEYCLOAK_ADMIN_PASSWORD=dbos-pass \
+docker run --rm  -d -p 8083:8080 --name keycloak --add-host=host.docker.internal:host-gateway \
+  -e KEYCLOAK_ADMIN=dbos-admin -e KEYCLOAK_ADMIN_PASSWORD=dbos-pass \
   -e KC_DB=postgres -e KC_DB_DATABASE=${BANK_DB_NAME} -e KC_DB_SCHEMA=keycloak \
   -e KC_DB_URL_DATABASE=${BANK_DB_NAME} -e KC_DB_PASSWORD=${PGPASSWORD} -e KC_DB_URL_HOST=${POSTGRES_HOST} -e KC_DB_URL_PORT=${POSTGRES_PORT} -e KC_DB_USERNAME=${BANK_DB_NAME} \
   -v ${SCRIPT_DIR}/dbos-realm.json:/opt/keycloak/data/import/realm.json \
-  quay.io/keycloak/keycloak:latest start-dev --import-realm
+  quay.io/keycloak/keycloak:latest start-dev --import-realm 
