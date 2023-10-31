2f1086579c8678ac0e2b91b93c26c5f7dcbd3d06
Harry Pierson <harry.pierson@dbos.dev>
2023-10-31 16:44:38 -0700

    generated baseline ts-fetch client

---
+++ bank/bank-frontend/client/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CreateAccountFuncRequest';
+export * from './Greeting200Response';
+export * from './ListAccountsFunc200ResponseInner';
+export * from './ListTxnForAccountFunc200ResponseInner';
