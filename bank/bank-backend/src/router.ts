139d0382e091f8205ab6af54b076a5c9f6910853
Harry Pierson <harry.pierson@dbos.dev>
2023-11-01 11:15:37 -0700

    remove TransactionHistoryParam

--- bank/bank-backend/src/router.ts
+++ bank/bank-backend/src/router.ts
@@ -3,18 +3,6 @@ import { BankTransactionHistory } from "./workflows/txnhistory.workflows";
 import { OperonResponseError, GetApi, HandlerContext, PostApi, DefaultRequiredRole, Authentication, KoaMiddleware } from "@dbos-inc/operon";
 import { bankAuthMiddleware, koaLogger, bankJwt } from "./middleware";
 
-// ts-json-schema-generator doesn't seem to handle the prisma TransactionHistory type
-// so declare a simple substitute to enable OpenAPI generation
-type TransactionHistoryParam = {
-  txnId: bigint;
-  fromAccountId: bigint;
-  fromLocation: string;
-  toAccountId: bigint;
-  toLocation: string;
-  amount: number;
-  timestamp: Date;
-}
-
 @DefaultRequiredRole(["appUser"])
 @Authentication(bankAuthMiddleware)
 @KoaMiddleware(koaLogger, bankJwt)
@@ -28,7 +16,7 @@ export class BankEndpoints {
 
   // Deposit.
   @PostApi("/api/deposit")
-  static async deposit(ctx: HandlerContext, history: TransactionHistoryParam) {
+  static async deposit(ctx: HandlerContext, history: TransactionHistory) {
     const data = convertTransactionHistory(history);
     if (!data.fromLocation) {
       throw new OperonResponseError("fromLocation must not be empty!", 400);
@@ -44,7 +32,7 @@ export class BankEndpoints {
 
   // Withdraw.
   @PostApi("/api/withdraw")
-  static async withdraw(ctx: HandlerContext, history: TransactionHistoryParam) {
+  static async withdraw(ctx: HandlerContext, history: TransactionHistory) {
     const data = convertTransactionHistory(history);
     if (!data.toLocation) {
       throw new OperonResponseError("toLocation must not be empty!", 400);
@@ -60,7 +48,7 @@ export class BankEndpoints {
 
   // Internal transfer
   @PostApi("/api/transfer")
-  static async internalTransfer(ctx: HandlerContext, history: TransactionHistoryParam) {
+  static async internalTransfer(ctx: HandlerContext, history: TransactionHistory) {
     const data = convertTransactionHistory(history);
     // Check the transaction is within the local database.
     if ((data.fromLocation !== undefined && data.fromLocation !== "local") || (data.toLocation !== undefined && data.toLocation !== "local")) {
@@ -78,7 +66,7 @@ export class BankEndpoints {
 
 // Helper functions to convert to the correct data types.
 // Especially convert the bigint.
-export function convertTransactionHistory(data: TransactionHistoryParam): TransactionHistory {
+export function convertTransactionHistory(data: TransactionHistory): TransactionHistory {
   if (!data.amount || data.amount <= 0.0) {
     throw new OperonResponseError("Invalid amount! " + data.amount, 400);
   }
