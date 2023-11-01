ea6bc143bcb564bb1bab1000d87b9fadd59ba1df
Chuck B <chuck.bear@dbos.dev>
2023-11-01 09:16:53 -0400

    Also get user ID from header

--- yky-social/src/app.ts
+++ yky-social/src/app.ts
@@ -65,14 +65,23 @@ export function getS3(ctx: OperonContext) {
 async function authMiddleware (ctx: MiddlewareContext) {
   if (ctx.requiredRole.length > 0) {
     const { userid } = ctx.koaContext.request.query;
-    const uid = userid?.toString();
+    let uid = userid;
+    if (!uid) {
+        uid = ctx.koaContext.headers['userid'];
+    }
+    const suid = uid?.toString();
+
+    if (!suid) {
+      const err = new Error.OperonNotAuthorizedError("Not logged in.", 401);
+      throw err;
+    }
 
     // We could do additional validation, such as a token.
     //  Currently the backend is trusting that the front end did that.
     //  But we will check that the database hasn't changed in a way
     //    that invalidates the frontend credentials. 
     const u = await ctx.query((dbclient: EntityManager) => {
-       return dbclient.getRepository(UserLogin).findOneBy({id: uid});
+       return dbclient.getRepository(UserLogin).findOneBy({id: suid});
     });
 
     if (!u || !u.active) {
@@ -80,12 +89,8 @@ async function authMiddleware (ctx: MiddlewareContext) {
       throw err;
     }
 
-    if (!uid) {
-      const err = new Error.OperonNotAuthorizedError("Not logged in.", 401);
-      throw err;
-    }
     return {
-      authenticatedUser: uid,
+      authenticatedUser: suid,
       authenticatedRoles: ['user']
     };
   }
--- yky-social/yky/app/dogetmediakey/route.tsx
+++ yky-social/yky/app/dogetmediakey/route.tsx
@@ -1,25 +1,18 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { getuserid } from '@/app/components/userid';
-import { getAPIServer } from '@/app/components/backend';
 
-export async function GET(request: NextRequest) {
+import { api, ResponseError } from '@/app/components/backend';
+
+export async function GET(_request: NextRequest) {
   const userid = getuserid();
 
-  const res = await fetch(getAPIServer() + '/getProfilePhoto'+'?' + new URLSearchParams({
-    userid: userid,
-  }),
-  {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
- 
-  if (res.ok) {
-    const data = await res.json();
-    return NextResponse.json(data);
+  try {
+    return NextResponse.json(await api.getProfilePhoto({
+      headers: {'userid': userid}
+    }));
   }
-  else {
-    return NextResponse.json({error: "Error", status:res.status});
+  catch (err) {
+    const e = err as ResponseError;
+    return NextResponse.json({}, e.response);
   }
 }
