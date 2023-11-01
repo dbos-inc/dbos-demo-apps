0d2fbc7b8bc23502b266651a999e2974108d6fd5
Chuck B <chuck.bear@dbos.dev>
2023-11-01 14:53:47 -0400

    API, doesn't work yet

--- yky-social/yky/app/components/backend.ts
+++ yky-social/yky/app/components/backend.ts
@@ -1,4 +1,6 @@
+import { NextResponse, NextRequest } from 'next/server';
 import { Configuration, DefaultApi } from '@/app/components/client';
+import { ResponseError } from '@/app/components/client';
 
 export { ResponseError } from '@/app/components/client';
 
@@ -12,3 +14,12 @@ const config = new Configuration({
 
 export const api = new DefaultApi(config);
 
+export async function placeApiRequest(request: NextRequest, func: (bapi: DefaultApi, req: NextRequest) => unknown ) {
+  try {
+    return NextResponse.json(await func(api, request));
+  }
+  catch (err) {
+    const e = err as ResponseError;
+    return NextResponse.json({}, e.response);
+  }
+}
