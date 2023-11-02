8a3811b6a6b7de6bdb9111edc41e3f8a684f7d73
Harry Pierson <harry.pierson@dbos.dev>
2023-11-01 17:33:30 -0700

    remove openapitools ts-fetch client

--- e-commerce/shop-frontend/client/models/index.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AddToCartRequest';
-export * from './GetCart200ResponseInner';
-export * from './GetCartRequest';
-export * from './GetProducts200ResponseInner';
-export * from './LoginRequest';
