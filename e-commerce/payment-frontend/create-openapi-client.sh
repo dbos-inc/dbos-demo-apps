7c3a9679f93a4c5d19029e6425e5f9de53e2e39a
Harry Pierson <harry.pierson@dbos.dev>
2023-11-02 10:48:23 -0700

    add create-openapi-client and oazapfts to payment frontend

--- e-commerce/payment-frontend/create-openapi-client.sh
+++ e-commerce/payment-frontend/create-openapi-client.sh
@@ -2,10 +2,10 @@ fe_dir=$(realpath $(dirname $0))
 cur_dir=$(pwd)
 
 # generate the openapi definition document
-cd $fe_dir/../shop-backend
+cd $fe_dir/../payment-backend
 npx operon openapi src/operations.ts
 
 cd $fe_dir
-npx oazapfts ../shop-backend/src/openapi.yaml  ./lib/client.ts
+npx oazapfts ../payment-backend/src/openapi.yaml  ./client.ts
 
 cd $cur_dir
