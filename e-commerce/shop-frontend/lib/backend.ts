47744ad29da2e72cdd7e6082803d444c8b728831
Harry Pierson <harry.pierson@dbos.dev>
2023-11-01 17:45:37 -0700

    API type by utility type instead of manual

--- e-commerce/shop-frontend/lib/backend.ts
+++ e-commerce/shop-frontend/lib/backend.ts
@@ -1,5 +1,6 @@
 import { AsyncReturnType } from "oazapfts";
 import * as $api from "./client";
+import { RequestOpts } from "oazapfts/lib/runtime";
 
 // Utility types to extract the return type of the oazapfts generated function
 type DataFieldType<T> = T extends { data: infer U } ? U : never
@@ -12,14 +13,5 @@ $api.defaults.baseUrl = backendAddress;
 export type CartProduct = ArrayItemType<OazapftsReturn<typeof $api.getCart>>
 export type Product = OazapftsReturn<typeof $api.getProduct>
 
-
-export const api = {
-    addToCart: $api.addToCart,
-    getCart: $api.getCart,
-    getProduct: $api.getProduct,
-    getProducts: $api.getProducts,
-    login: $api.login,
-    paymentWebhook: $api.paymentWebhook,
-    register: $api.register,
-    webCheckout: $api.webCheckout,
-}
+// hide defaults and servers from publicly exported api object
+export const api: Omit<typeof $api, 'defaults' | 'servers'> = $api;
