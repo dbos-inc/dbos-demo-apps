355f1c477702bfd1a5d8a7344a2787145ce3cba1
chuck-dbos <134347445+chuck-dbos@users.noreply.github.com>
2023-11-07 15:24:53 -0500

    Clean up YKY post page (#86)
    Also removes an unused server-render timeline component

---
+++ yky-social/yky/app/main/post/[id]/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { formatDistanceToNow } from 'date-fns';
+import { getPageData } from '@/app/components/backend';
+
+export default async function Page({
+    params,
+  } : {
+    params: { id: string },
+  })
+{
+  try {
+    const data = await getPageData((api, headers) => {return api.getPost({id: params.id}, headers);});
+
+    return (
+      <div className="p-4 bg-white shadow-lg rounded-md">
+        <div className="flex items-center justify-between mb-2">
+              <div className="text-xl font-bold text-gray-900">{data.post.authorUser.user_name}</div>
+              <div className="text-sm text-gray-500">{formatDistanceToNow(new Date(data.post.post_time), {addSuffix: true})}</div>
+          </div>
+          <div className="mb-2 text-gray-700">{data.post.text}</div>
+      </div>
+    );
+  }
+  catch (e) {
+    return (
+      <h1>Post does not exist.</h1>
+    );
+  }
+}
