2a7adcba8e4c182c540f576e311c4345fca0f744
Chuck B <chuck.bear@dbos.dev>
2023-11-13 12:22:45 -0500

    Start linting bad code

--- badcode/src/operations.ts
+++ badcode/src/operations.ts
@@ -1,6 +1,21 @@
 import { TransactionContext, OperonTransaction, GetApi, ArgSource, ArgSources } from '@dbos-inc/operon';
 import { Knex } from 'knex';
 
+
+export const settings = {
+    theme: 'dark',
+    notifications: true,
+    language: 'en'
+};
+
+function getUserSetting(setting): string {
+    return eval('settings.' + setting) as string;
+}
+
+// User input
+const userSetting = 'language';
+console.log(getUserSetting(userSetting)); // Outputs 'en'
+
 // The schema of the database table used in this example.
 export interface operon_hello {
   name: string;
@@ -17,5 +32,10 @@ export class Hello {
     const { rows } = await ctxt.client.raw(query, [user]) as { rows: operon_hello[] };
     const greet_count = rows[0].greet_count;
     return `Hello, ${user}! You have been greeted ${greet_count} times.\n`;
+  }
+
+  @GetApi('/setting/:setting')
+  static async getSetting(_ctx: HandlerContext, @ArgSource(ArgSources.URL) setting: string) {
+    return getUserSetting(setting);
   }
 }
