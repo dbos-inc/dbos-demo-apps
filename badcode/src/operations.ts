d9d95a5a5d499bbf2f7a29d5b86e197f84b74084
Chuck B <chuck.bear@dbos.dev>
2023-11-15 11:41:57 -0500

    Add some more things to find

--- badcode/src/operations.ts
+++ badcode/src/operations.ts
@@ -9,6 +9,7 @@ import {
 
 import { Knex } from 'knex';
 
+export const AWS_TOKEN = "AKIAIUWUUQQN3GNUA88V";
 
 export const settings = {
     theme: 'dark',
@@ -17,12 +18,9 @@ export const settings = {
 };
 
 function getUserSetting(setting: string): string {
+    debugger;
     return eval('settings.' + setting) as string;
 }
-
-// User input
-const userSetting = 'language';
-console.log(getUserSetting(userSetting)); // Outputs 'en'
 
 // The schema of the database table used in this example.
 export interface operon_hello {
@@ -41,11 +39,24 @@ export class Hello {
     const query = "INSERT INTO operon_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = operon_hello.greet_count + 1 RETURNING greet_count;";
     const { rows } = await ctxt.client.raw(query, [user]) as { rows: operon_hello[] };
     const greet_count = rows[0].greet_count;
+    console.log(`Hello, ${user}! You have been greeted ${greet_count} times.`); // Even worse if we give this side-effects
     return `Hello, ${user}! You have been greeted ${greet_count} times.\n`;
   }
 
   @GetApi('/setting/:setting')
   static async getSetting(_ctx: HandlerContext, @ArgSource(ArgSources.URL) setting: string) {
     return getUserSetting(setting);
+  }
+
+  @GetApi('/register')
+  static async createAcct(_ctx: HandlerContext, username: string, password: string) {
+    //const complexityregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$/;
+    const complexityregex = /^[A-Za-z.,'-]([A-Za-z.,' -]*[A-Za-z.,'-])?$/;
+    if (complexityregex.test(password)) {
+      return {user: username, pass: password};
+    }
+    else {
+      throw new Error("Password not strong enough");
+    }
   }
 }
