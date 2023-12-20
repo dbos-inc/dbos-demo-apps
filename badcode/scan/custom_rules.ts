99f80f8563b081f2816f2c73b73b27144e271777
Chuck B <chuck.bear@dbos.dev>
2023-12-20 11:19:27 -0500

    Start custom rules for ND functions

---
+++ badcode/scan/custom_rules.ts
@@ -0,0 +1,52 @@
+module.exports = {
+  rules: {
+    'dbos-rules/detect-nondeterministic-calls': {
+      // Rule configuration for Math.random() detection
+      meta: {
+        type: 'suggestion',
+        docs: {
+          description: 'Detect calls to nondeterministic functions like Math.random(), which should be called via DBOS rather than directly',
+        },
+        schema: [],
+      },
+      create: function (context) {
+        return {
+          CallExpression(node) {
+            if (node.callee.type === 'MemberExpression' &&
+                node.callee.object.name === 'Math' &&
+                node.callee.property.name === 'random')
+	    {
+              context.report({
+                node: node,
+                message: 'Avoid calling Math.random() directly; it can lead to non-reproducible behavior.',
+              });
+            }
+          },
+        };
+      },
+    },
+    'dbos-rules/detect-new-date': {
+      // Rule configuration for new Date() detection
+      meta: {
+        type: 'suggestion',
+        docs: {
+          description: 'Detect calls to new Date(), which should be called via DBOS rather than directly',
+        },
+        schema: [],
+      },
+      create: function (context) {
+        return {
+          NewExpression(node) {
+            if (node.callee.name === 'Date') {
+              context.report({
+                node: node,
+                message: 'Avoid using new Date(); consider using the DBOS XXX function for consistency and testability.',
+              });
+            }
+          },
+        };
+      },
+    },
+  },
+};
+
