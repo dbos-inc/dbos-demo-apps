544456bb152249bd83cffbc2ffa3ac6392a7613f
Chuck B <chuck.bear@dbos.dev>
2024-01-09 11:03:14 -0500

    Add lint, fix issues found

--- hello-extended/src/operations.ts
+++ hello-extended/src/operations.ts
@@ -1,4 +1,4 @@
-import { TransactionContext, Transaction, GetApi, PostApi, CommunicatorContext, Communicator, Workflow, WorkflowContext, ArgSource, ArgSources } from '@dbos-inc/dbos-sdk'
+import { TransactionContext, Transaction, GetApi, PostApi, CommunicatorContext, Communicator, Workflow, WorkflowContext, ArgSource, ArgSources } from '@dbos-inc/dbos-sdk';
 import { Knex } from 'knex';
 
 export interface dbos_hello {
@@ -25,7 +25,7 @@ export class Hello {
   @Transaction()
   static async helloTransaction(ctxt: TransactionContext<Knex>, user: string) {
     // Retrieve and increment the number of times this user has been greeted.
-    const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;"
+    const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
     const { rows } = await ctxt.client.raw(query, [user]) as { rows: dbos_hello[] };
     const greet_count = rows[0].greet_count;
     return `Hello, ${user}! You have been greeted ${greet_count} times.\n`;
