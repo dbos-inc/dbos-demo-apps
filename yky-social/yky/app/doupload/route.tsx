b43bf9ca81790097768c24c84a6f51c4d9bcf8cb
Chuck B <chuck.bear@dbos.dev>
2024-01-29 16:45:35 -0500

    No workflow key

--- yky-social/yky/app/doupload/route.tsx
+++ yky-social/yky/app/doupload/route.tsx
@@ -4,6 +4,6 @@ import { placeApiRequest } from '@/app/components/backend';
 
 export async function GET(request: NextRequest) {
   return await placeApiRequest(request, async (api, req, hdrs) => {
-    return await api.doStartMediaUpload(await req.json(), hdrs);
+    return await api.doStartMediaUpload({}, hdrs);
   })
 }
--- yky-social/yky/app/fetchstl/route.tsx
+++ yky-social/yky/app/fetchstl/route.tsx
@@ -3,6 +3,6 @@ import { placeApiRequest } from '@/app/components/backend';
 
 export async function GET(request: NextRequest) {
   return placeApiRequest(request, async (api, req, hdrs) => {
-    return await api.sendTimeline(await req.json(), hdrs);
+    return await api.sendTimeline({}, hdrs);
   });
 }
