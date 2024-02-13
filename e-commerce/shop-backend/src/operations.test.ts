6ce43aab6abd9c71eb20386f966d5dd2f004d569
Chuck B <chuck.bear@dbos.dev>
2024-02-12 19:54:06 -0500

    Test add to cart

--- e-commerce/shop-backend/src/operations.test.ts
+++ e-commerce/shop-backend/src/operations.test.ts
@@ -77,9 +77,95 @@ describe("operations", () => {
     const bpresp2 = await request(testRuntime.getHandlersCallback())
       .get(`/api/products/9801`);
     expect(bpresp2.status).toBe(204);
-});
+  });
 
-  // products/:id
+  test("shopping", async () => {
+    /* CB - This is probably a bug - gets a 204.
+    const bacr = {'username': 'noshopper', 'product_id':1};
+    const bcresp = await request(testRuntime.getHandlersCallback())
+      .post("/api/add_to_cart")
+      .send(bacr);
+    expect(bcresp.status).toBe(400); */
+
+    /* CB - This is probably a bug too - gets a 204
+    const bacr2 ={'username': 'shopper', 'product_id':9801};
+    const bcresp2 = await request(testRuntime.getHandlersCallback())
+      .post("/api/add_to_cart")
+      .send(bacr2);
+    expect(bcresp2.status).toBe(400);
+    */
+
+    const acr ={'username': 'shopper', 'product_id':1};
+    const cresp = await request(testRuntime.getHandlersCallback())
+      .post("/api/add_to_cart")
+      .send(acr);
+    expect(cresp.status).toBe(204);
+  });
+
+  /*
+    # Add an item to your cart
+    url = f'https://{config.dbos_domain}/{config.default_username}/application/{shopapp}/api/add_to_cart'
+    data = {'username': shop_username, 'product_id':1}
+    res = session.post(url, json=data, timeout=config.default_timeout)
+    assert res.status_code == 204
+
+    # Retrieve your payment session
+    url = f'https://{config.dbos_domain}/{config.default_username}/application/{paymentapp}/api/session/1'
+    res = session.get(url, timeout=config.default_timeout)
+    assert res.status_code == 204
+
+    # Retrieve your cart
+    url = f'https://{config.dbos_domain}/{config.default_username}/application/{shopapp}/api/get_cart'
+    data = {'username': shop_username}
+    res = session.post(url, json=data, timeout=config.default_timeout)
+    assert res.status_code == 200
+    assert (len(res.json()) == 1)
+
+    # Check out your cart
+    headers = {"Origin": "xxx"}
+    url = f'https://{config.dbos_domain}/{config.default_username}/application/{shopapp}/api/checkout_session?username={shop_username}'
+    res = session.post(url, headers = headers, allow_redirects=False, timeout=120)
+    content = res.content.decode('utf-8')
+    session_id = extract_uuid_from_line(content)
+    assert session_id is not None, content
+
+    # Submit your payment
+    sessiondata = {"session_id":session_id}
+    url = f'https://{config.dbos_domain}/{config.default_username}/application/{paymentapp}/api/submit_payment'
+    res = session.post(url, json=sessiondata, timeout=config.default_timeout)
+
+    # After some time, your payment should succeed
+    payment_successful = False
+    for _ in range(10):
+        url = f'https://{config.dbos_domain}/{config.default_username}/application/{paymentapp}/api/session/{session_id}'
+        res = requests.get(url, timeout=config.default_timeout)
+        try:
+            data = res.json()
+            if data['payment_status'] == "paid":
+                payment_successful = True
+                break
+        except ValueError:
+            pass
+        time.sleep(1)
+    assert payment_successful
+
+    # After the payment has succeeded, your cart should be emptied
+    cart_empty = False
+    for _ in range(10):
+        url = f'https://{config.dbos_domain}/{config.default_username}/application/{shopapp}/api/get_cart'
+        data = {'username': shop_username}
+        res = session.post(url, json=data, timeout=config.default_timeout)
+        try:
+            data = res.json()
+            if len(data) == 0:
+                cart_empty = True
+                break
+        except ValueError:
+            pass
+        time.sleep(1)
+    assert cart_empty
+   */
+
   // add_to_cart
   // get_cart
   // checkout_session
