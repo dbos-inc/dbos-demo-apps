1b39efdf3b9988ea3bb368090e16215885f25968
Max dml <maxdemoulin@gmail.com>
2024-02-25 18:27:18 -0800

    add greeting emails app (#118)
    Add the code supporting the new example at
    https://docs.dbos.dev/category/quickstart-programming

---
+++ greeting-emails/src/operations.ts
@@ -0,0 +1,49 @@
+import {
+    TransactionContext, Transaction,
+    HandlerContext, GetApi,
+    CommunicatorContext, Communicator,
+    WorkflowContext, Workflow,
+} from "@dbos-inc/dbos-sdk";
+import { Knex } from "knex";
+
+export class Greetings {
+    @Communicator()
+    static async SendGreetingEmail(ctxt: CommunicatorContext) {
+        ctxt.logger.info("Sending Email...");
+        // Code omitted for simplicity
+        ctxt.logger.info("Email sent!");
+        await new Promise((resolve) => resolve("Email sent!"));
+    }
+
+    @Transaction()
+    static async InsertGreeting(ctxt: TransactionContext<Knex>, friend: string, content: string) {
+        await ctxt.client.raw(
+            "INSERT INTO dbos_greetings (greeting_name, greeting_note_content) VALUES (?, ?)",
+            [friend, content]
+        );
+    }
+
+    @Workflow()
+    static async SendGreetingNoteWorkflow(ctxt: WorkflowContext, friend: string) {
+        const noteContent = `Thank you for being awesome, ${friend}!`;
+        await ctxt.invoke(Greetings).SendGreetingEmail();
+
+        for (let i = 0; i < 5; i++) {
+            ctxt.logger.info(
+                "Press control + C to interrupt the workflow..."
+            );
+            // Sleep 1 second
+            await ctxt.sleep(1);
+        }
+
+        await ctxt.invoke(Greetings).InsertGreeting(friend, noteContent);
+        ctxt.logger.info("Workflow done!");
+        return noteContent;
+    }
+
+    @GetApi("/greeting/:friend")
+    static async Greet(ctxt: HandlerContext, friend: string) {
+        const workflow_handle = await ctxt.invoke(Greetings).SendGreetingNoteWorkflow(friend);
+        return await workflow_handle.getResult();
+    }
+}
