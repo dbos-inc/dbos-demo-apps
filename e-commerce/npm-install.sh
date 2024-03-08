09fbca89f5b7bbfc8c7e5008fc50bec14cfda467
Chuck B <chuck.bear@dbos.dev>
2024-03-08 15:00:38 -0500

    Remove payment-frontend reference

--- e-commerce/npm-install.sh
+++ e-commerce/npm-install.sh
@@ -3,4 +3,3 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 npm install --prefix $SCRIPT_DIR/shop-backend
 npm install --prefix $SCRIPT_DIR/shop-frontend
 npm install --prefix $SCRIPT_DIR/payment-backend
-npm install --prefix $SCRIPT_DIR/payment-frontend
