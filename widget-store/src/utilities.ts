dc38e2ffc409badb8ff6a695b4f0117637cd8efd
Harry Pierson <harrypierson@outlook.com>
2024-04-11 09:39:12 -0700

    format + lint fixes + decorating read only queries correctly

--- widget-store/src/utilities.ts
+++ widget-store/src/utilities.ts
@@ -1,15 +1,13 @@
-import {
-    Transaction, Communicator, CommunicatorContext, TransactionContext, HandlerContext, PostApi, GetApi, DBOSResponseError
-} from '@dbos-inc/dbos-sdk';
+import { Transaction, TransactionContext} from '@dbos-inc/dbos-sdk';
 import { Knex } from 'knex';
 
 type KnexTransactionContext = TransactionContext<Knex>;
 
 export enum OrderStatus {
-  PENDING= 0,
-  FULFILLED= 1,
-  CANCELLED= -1,
-};
+  PENDING = 0,
+  FULFILLED = 1,
+  CANCELLED = -1,
+}
 
 export interface Product {
   product_id: number,
@@ -26,21 +24,21 @@ export interface Order {
   product_id: number,
 }
 
-export const PRODUCT_ID = 1
+export const PRODUCT_ID = 1;
 
 export class ShopUtilities {
   @Transaction()
   static async subtractInventory(ctxt: KnexTransactionContext): Promise<void> {
-      const numAffected = await ctxt.client<Product>('products').where('product_id', PRODUCT_ID).andWhere('inventory', '>=', 1)
+    const numAffected = await ctxt.client<Product>('products').where('product_id', PRODUCT_ID).andWhere('inventory', '>=', 1)
       .update({
         inventory: ctxt.client.raw('inventory - ?', 1)
       });
-      //A good block to uncomment in time-travel debugger to see an example of querying past state
-      //const item = await ctxt.client<Product>('products').select('inventory').where({ product_id: product.product_id });
-      //ctxt.logger.info(">>> Remaining inventory: " + item[0].inventory); 
-      if (numAffected <= 0) {
-        throw new Error("Insufficient Inventory");
-      }
+    //A good block to uncomment in time-travel debugger to see an example of querying past state
+    //const item = await ctxt.client<Product>('products').select('inventory').where({ product_id: product.product_id });
+    //ctxt.logger.info(">>> Remaining inventory: " + item[0].inventory); 
+    if (numAffected <= 0) {
+      throw new Error("Insufficient Inventory");
+    }
   }
 
   @Transaction()
@@ -48,7 +46,7 @@ export class ShopUtilities {
     await ctxt.client<Product>('products').where({ product_id: PRODUCT_ID }).update({ inventory: ctxt.client.raw('inventory + ?', 1) });
   }
 
-  @Transaction()
+  @Transaction({ readOnly: true })
   static async retrieveProduct(ctxt: KnexTransactionContext): Promise<Product> {
     const item = await ctxt.client<Product>('products').select("*").where({ product_id: PRODUCT_ID });
     if (!item.length) {
@@ -59,17 +57,18 @@ export class ShopUtilities {
 
   @Transaction()
   static async createOrder(ctxt: KnexTransactionContext): Promise<number> {
-    const orders = await ctxt.client<Order>('orders').insert({ 
-      order_status: OrderStatus.PENDING, 
+    const orders = await ctxt.client<Order>('orders').insert({
+      order_status: OrderStatus.PENDING,
       product_id: PRODUCT_ID,
-      last_update_time: ctxt.client.fn.now()}).returning('order_id');
+      last_update_time: ctxt.client.fn.now()
+    }).returning('order_id');
     const orderID = orders[0].order_id;
     return orderID;
   }
 
   @Transaction()
   static async fulfillOrder(ctxt: KnexTransactionContext, order_id: number): Promise<void> {
-    await ctxt.client<Order>('orders').where({ order_id: order_id }).update({ 
+    await ctxt.client<Order>('orders').where({ order_id: order_id }).update({
       order_status: OrderStatus.FULFILLED,
       last_update_time: ctxt.client.fn.now()
     });
@@ -77,13 +76,13 @@ export class ShopUtilities {
 
   @Transaction()
   static async errorOrder(ctxt: KnexTransactionContext, order_id: number): Promise<void> {
-    await ctxt.client<Order>('orders').where({ order_id: order_id }).update({ 
+    await ctxt.client<Order>('orders').where({ order_id: order_id }).update({
       order_status: OrderStatus.CANCELLED,
-      last_update_time: ctxt.client.fn.now() 
+      last_update_time: ctxt.client.fn.now()
     });
   }
 
-  @Transaction()
+  @Transaction({ readOnly: true })
   static async retrieveOrder(ctxt: KnexTransactionContext, order_id: number): Promise<Order> {
     const item = await ctxt.client<Order>('orders').select("*").where({ order_id: order_id });
     if (!item.length) {
