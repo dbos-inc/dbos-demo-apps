4aeba750a7c3017ac9bbf10259f13fc65838fff3
Chuck B <chuck.bear@dbos.dev>
2024-06-25 07:49:53 -0400

    Re-add

---
+++ bank/bank-backend/src/middleware.ts
@@ -0,0 +1,44 @@
+import { MiddlewareContext, DBOSResponseError } from "@dbos-inc/dbos-sdk";
+import { koaJwtSecret } from "jwks-rsa";
+import jwt from "koa-jwt";
+import logger from "koa-logger";
+
+// If we put these middleware functions in router.ts, txnhistory and accountinfo classes will get undefined middlewares due to circular dependencies.
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function bankAuthMiddleware(ctx: MiddlewareContext) {
+  // Only extract user and roles if the operation specifies required roles.
+  if (ctx.requiredRole.length > 0) {
+    //console.log("required role: ", ctx.requiredRole);
+    if (!ctx.koaContext.state.user) {
+      throw new DBOSResponseError("No authenticated user!", 401);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const authenticatedUser: string = ctx.koaContext.state.user["preferred_username"] ?? "";
+    //console.log("current user: ", authenticatedUser);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const authenticatedRoles: string[] = ctx.koaContext.state.user["realm_access"]["roles"] ?? [];
+    //console.log("JWT claimed roles: ", authenticatedRoles);
+    if (authenticatedRoles.includes("appAdmin")) {
+      // appAdmin role has more priviledges than appUser.
+      authenticatedRoles.push("appUser");
+    }
+    //console.log("authenticated roles: ", authenticatedRoles);
+    return { authenticatedUser: authenticatedUser, authenticatedRoles: authenticatedRoles };
+  }
+}
+
+export const bankJwt = jwt({
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  // Note: We have to read the config from env not from a file, because decorators are loaded before the DBOS executor, we must have the variables available during the loading time.
+  secret: koaJwtSecret({
+    jwksUri: `http://${process.env.BANK_HOST || "localhost"}:${process.env.AUTH_PORT || "8083"}/realms/dbos/protocol/openid-connect/certs`,
+    cache: true,
+    cacheMaxEntries: 5,
+    cacheMaxAge: 600000,
+  }),
+  issuer: `http://${process.env.BANK_HOST || "localhost"}:${process.env.AUTH_PORT || "8083"}/realms/dbos`,
+});
+
+export const koaLogger = logger();
