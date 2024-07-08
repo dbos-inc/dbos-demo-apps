9055abfcb9e851ef1f7ab60edfea1e1149140258
Chuck B <chuck.bear@dbos.dev>
2024-07-08 16:32:38 -0400

    Start frontend

--- widget-fulfillment/src/frontend.ts
+++ widget-fulfillment/src/frontend.ts
@@ -1,11 +1,10 @@
-import { GetApi, HandlerContext } from "@dbos-inc/dbos-sdk";
+import { GetApi, HandlerContext, PostApi } from "@dbos-inc/dbos-sdk";
 import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
 import { ShopUtilities, OrderStatus } from "./utilities";
 import { Liquid } from "liquidjs";
 
 const engine = new Liquid({
-  root: path.resolve(__dirname, '..', 'public'),
+  root: path.resolve(__dirname, '..', 'views'),
   extname: ".liquid"
 });
 
@@ -16,24 +15,43 @@ async function render(file: string, ctx?: object): Promise<string> {
 export class Frontend {
 
   @GetApi('/')
-  static async frontend(ctxt: HandlerContext) {
-    const prod = await ctxt.invoke(ShopUtilities).retrieveProduct();
-    return await render("purchase", {
-      uuid: uuidv4(),
-      inventory: prod.inventory,
-      product: prod.product,
-      description: prod.description,
-      price: prod.price,
-    });
+  static async userPick(_ctxt: HandlerContext) {
+    return await render("index", {});
   }
 
-  @GetApi('/payment/:key')
-  static payment(_ctxt: HandlerContext, key: string) {
-    return render("payment", {
-      uuid: key,
+  @PostApi('/')
+  static async setUser(ctxt: HandlerContext, name: string) {
+    ctxt.koaContext.redirect(`/fulfill?name=${encodeURIComponent(name)}`);
+    return Promise.resolve();
+  }
+
+  @GetApi('/fulfill')
+  static async payment(_ctxt: HandlerContext, name: string) {
+    return render("fulfill_order", {
+      name,
     });
   }
   
+  @PostApi('/fulfill/cancel')
+  static async cancelFulfill(ctxt: HandlerContext/*, name: string*/) {
+    ctxt.koaContext.redirect('/');
+    return Promise.resolve();
+  }
+  
+  @PostApi('/fulfill/fulfilled') 
+  static async completeFulfill(ctxt: HandlerContext/*, name: string*/) {
+    // Handle fulfillment logic
+    ctxt.koaContext.redirect('/');
+    return Promise.resolve();
+  }
+  
+  @PostApi('/fulfill/more_time')
+  static async extendFulfill(ctxt: HandlerContext, name: string) {
+    // Handle request for more time
+    ctxt.koaContext.redirect(`/fulfill?name=${encodeURIComponent(name)}`);
+    return Promise.resolve();
+  }
+
   @GetApi('/error')
   static error(_ctxt: HandlerContext) {
     return render("error", {});
@@ -42,16 +60,5 @@ export class Frontend {
   @GetApi('/crash')
   static crash(_ctxt: HandlerContext) {
     return render("crash", {});
-  }
-
-  @GetApi('/order/:order_id')
-  static async order(ctxt: HandlerContext, order_id: number) {
-    const order = await ctxt.invoke(ShopUtilities).retrieveOrder(order_id);
-    return await render("order_status", {
-      order_id: order.order_id,
-      status: OrderStatus[order.order_status],
-      time: order.last_update_time
-    });
-  }
-  
+  } 
 }
