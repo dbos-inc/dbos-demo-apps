51deab480701d97c46bb0e361fb196607acd91a0
Chuck B <chuck.bear@dbos.dev>
2024-07-08 20:04:43 -0400

    Beginning of widget fulfill

--- widget-fulfillment/seeds/initial.js
+++ widget-fulfillment/seeds/initial.js
@@ -1,16 +1,5 @@
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex("orders").del();
-  await knex("products").del();
-
-  // Inserts seed entries
-  await knex("products").insert([
-    {
-      product_id: 1,
-      product: "Premium Quality Widget",
-      description: "Enhance your productivity with our top-rated widgets!",
-      inventory: 12,
-      price: 99.99
-    },
-  ]);
+  await knex("order_packer").del();
+  await knex("packer").del();
 };
