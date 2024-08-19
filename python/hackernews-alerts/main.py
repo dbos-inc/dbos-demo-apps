cf6ca32ee6518eeb6cf2c4d6113bd78157b6bd2c
Peter Kraft <petereliaskraft@gmail.com>
2024-08-19 15:25:03 -0700

    slack

--- python/hackernews-alerts/main.py
+++ python/hackernews-alerts/main.py
@@ -1,7 +1,10 @@
 import html
+import os
+import re
 from datetime import UTC, datetime, timedelta
 
 import requests
+import slack_sdk
 
 from dbos import DBOS
 
@@ -23,15 +26,33 @@ def search_hackernews(query: str, window_size_hours: int):
 
     hits = []
     for hit in response["hits"]:
-        comment = html.unescape(hit["comment_text"])
+        comment = hit["comment_text"]
+        comment = re.sub("<p>", "\n", html.unescape(comment))
+        comment = re.sub("<[^<]+?>", "", comment)
         url = f"https://news.ycombinator.com/item?id={hit['objectID']}"
         hits.append((comment, url))
     return hits
 
 
-if __name__ == "__main__":
-    results = search_hackernews("serverless", 24)
+@dbos.communicator()
+def post_to_slack(comment: str, url: str):
+    message = f"""
+    {comment}
+
+    {url}
+    """
+    client = slack_sdk.WebClient(token=os.environ["SLACK_HN_BOT_OAUTH_TOKEN"])
+    client.chat_postMessage(
+        channel="hacker-news-alerts",
+        text=message,
+        unfurl_links=False,
+        unfurl_media=False,
+    )
+
+
+@dbos.scheduled("0 * * * *")
+def run_hourly(scheduled_time: datetime, actual_time: datetime):
+    results = search_hackernews("serverless", window_size_hours=1)
     for comment, url in results:
-        print(comment)
-        print(url)
-    dbos.destroy()
+        post_to_slack(comment, url)
+    DBOS.logger.info(f"Found {len(results)} comments at {str(actual_time)}")
