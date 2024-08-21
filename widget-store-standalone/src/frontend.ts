868002e069d33a0a74276f7b41890811ac870fa1
Leander Neiss <1871704+lanice@users.noreply.github.com>
2024-08-21 17:08:53 -0400

    use public/main.html as entry

--- widget-store-standalone/src/frontend.ts
+++ widget-store-standalone/src/frontend.ts
@@ -1,22 +1,26 @@
 import { GetApi, HandlerContext } from "@dbos-inc/dbos-sdk";
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
 import { ShopUtilities, OrderStatus } from "./utilities";
 import { Liquid } from "liquidjs";
 
 const engine = new Liquid({
-  root: path.resolve(__dirname, '..', 'public'),
-  extname: ".liquid"
+  root: path.resolve(__dirname, "..", "public"),
+  extname: ".liquid",
 });
 
 async function render(file: string, ctx?: object): Promise<string> {
-  return await engine.renderFile(file, ctx) as string;
+  return (await engine.renderFile(file, ctx)) as string;
 }
 
 export class Frontend {
+  @GetApi("/")
+  static frontend(_ctxt: HandlerContext) {
+    return render("main.html", {});
+  }
 
-  @GetApi('/')
-  static async frontend(ctxt: HandlerContext) {
+  @GetApi("/purchase")
+  static async purchase(ctxt: HandlerContext) {
     const prod = await ctxt.invoke(ShopUtilities).retrieveProduct();
     return await render("purchase", {
       uuid: uuidv4(),
@@ -27,31 +31,30 @@ export class Frontend {
     });
   }
 
-  @GetApi('/payment/:key')
+  @GetApi("/payment/:key")
   static payment(_ctxt: HandlerContext, key: string) {
     return render("payment", {
       uuid: key,
     });
   }
 
-  @GetApi('/error')
+  @GetApi("/error")
   static error(_ctxt: HandlerContext) {
     return render("error", {});
   }
 
-  @GetApi('/crash')
+  @GetApi("/crash")
   static crash(_ctxt: HandlerContext) {
     return render("crash", {});
   }
 
-  @GetApi('/order/:order_id')
+  @GetApi("/order/:order_id")
   static async order(ctxt: HandlerContext, order_id: number) {
     const order = await ctxt.invoke(ShopUtilities).retrieveOrder(order_id);
     return await render("order_status", {
       order_id: order.order_id,
       status: OrderStatus[order.order_status],
-      time: order.last_update_time
+      time: order.last_update_time,
     });
   }
-
 }
