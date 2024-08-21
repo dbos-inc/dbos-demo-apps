0d8de10b23cfd693e9b366a15160ddb0db36dae6
Peter Kraft <petereliaskraft@gmail.com>
2024-08-21 13:07:24 -0700

    better

--- python/earthquake-tracker/streamlit.py
+++ python/earthquake-tracker/streamlit.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc, select
+from schema import earthquake_tracker
+from dbos.dbos_config import load_config, ConfigFile
 
-
+config: ConfigFile = load_config()
 
 # Database connection parameters
 db_params = {
-    'host': 'localhost',
-    'database': 'earthquake_tracker',
-    'user': 'postgres',
-    'password': 'dbos',
-    'port': '5432'
+    'host': config["database"]["hostname"],
+    'database': config["database"]["app_db_name"],
+    'user': config["database"]["username"],
+    'password': config["database"]["password"],
+    'port': config["database"]["port"]
 }
 
 engine = create_engine(f"postgresql://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['database']}")
@@ -19,10 +21,13 @@ engine = create_engine(f"postgresql://{db_params['user']}:{db_params['password']
 if __name__ == "__main__":
     st.title("Postgres Table Display")
 
-    query = "SELECT * FROM earthquake_tracker"
+    query = select(earthquake_tracker).order_by(desc(earthquake_tracker.c.timestamp)).limit(1000)
 
     # Fetch data
     df = pd.read_sql(query, engine)
+    df['timestamp'] = pd.to_numeric(df['timestamp'], errors='coerce')
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+    df = df.drop(columns=['id'])
 
     # Display the dataframe as a table
     st.table(df)
