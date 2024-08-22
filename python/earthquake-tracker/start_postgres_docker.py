80e24e040e4e75edcf1fa52e74db3fabf3eb524d
Peter Kraft <petereliaskraft@gmail.com>
2024-08-22 08:24:15 -0700

    tweaks

--- python/earthquake-tracker/start_postgres_docker.py
+++ python/earthquake-tracker/start_postgres_docker.py
@@ -1,29 +1,38 @@
 import os
+import subprocess
 import sys
 import time
-import subprocess
 
 # Default PostgreSQL port
-port = '5432'
+port = "5432"
 
 # Set the host PostgreSQL port with the -p/--port flag
 for i, arg in enumerate(sys.argv):
-    if arg in ['-p', '--port']:
+    if arg in ["-p", "--port"]:
         if i + 1 < len(sys.argv):
             port = sys.argv[i + 1]
 
-if 'PGPASSWORD' not in os.environ:
+if "PGPASSWORD" not in os.environ:
     print("Error: PGPASSWORD is not set.")
     sys.exit(1)
 
 try:
-    subprocess.run([
-        'docker', 'run', '--rm', '--name=dbos-db',
-        f'--env=POSTGRES_PASSWORD={os.environ["PGPASSWORD"]}',
-        '--env=PGDATA=/var/lib/postgresql/data',
-        '--volume=/var/lib/postgresql/data',
-        '-p', f'{port}:5432', '-d', 'ankane/pgvector'
-    ], check=True)
+    subprocess.run(
+        [
+            "docker",
+            "run",
+            "--rm",
+            "--name=dbos-db",
+            f'--env=POSTGRES_PASSWORD={os.environ["PGPASSWORD"]}',
+            "--env=PGDATA=/var/lib/postgresql/data",
+            "--volume=/var/lib/postgresql/data",
+            "-p",
+            f"{port}:5432",
+            "-d",
+            "ankane/pgvector",
+        ],
+        check=True,
+    )
 
     print("Waiting for PostgreSQL to start...")
     attempts = 30
@@ -31,8 +40,18 @@ try:
     while attempts > 0:
         try:
             subprocess.run(
-                ['docker', 'exec', 'dbos-db', 'psql', '-U', 'postgres', '-c', 'SELECT 1;'],
-                check=True, capture_output=True
+                [
+                    "docker",
+                    "exec",
+                    "dbos-db",
+                    "psql",
+                    "-U",
+                    "postgres",
+                    "-c",
+                    "SELECT 1;",
+                ],
+                check=True,
+                capture_output=True,
             )
             print("PostgreSQL started!")
             print("Database started successfully!")
@@ -40,7 +59,7 @@ try:
         except subprocess.CalledProcessError:
             attempts -= 1
             time.sleep(1)
-    
+
     if attempts == 0:
         print("Failed to start PostgreSQL.")
 
--- python/hackernews-alerts/README.md
+++ python/hackernews-alerts/README.md
@@ -2,6 +2,8 @@
 
 This app (inspired by [Modal](https://modal.com/docs/examples/hackernews_alerts)) uses DBOS to deploy a cron job that periodically searches Hacker News for people commenting about serverless computing and posts the comments to Slack.
 
+### Setting Up Slack
+
 To run the app, you need a Slack bot token.
 Follow this tutorial to generate one: https://api.slack.com/tutorials/tracks/getting-a-token
 Your token should start with "xoxb".
@@ -13,7 +15,9 @@ export SLACK_HN_BOT_OAUTH_TOKEN=<your_token>
 
 You should also create a Slack channel named `hacker-news-alerts` for the bot to post to!
 
-To deploy this app to DBOS Cloud, first install the DBOS Cloud CLI (requires Node):
+### Deploying to the Cloud
+
+To deploy this app as a persistent cron job to DBOS Cloud, first install the DBOS Cloud CLI (requires Node):
 
 ```shell
 npm i -g @dbos-inc/dbos-cloud
@@ -25,8 +29,9 @@ Then, run this command to deploy your app:
 dbos-cloud app deploy
 ```
 
-This command will output your app's URL--visit it to see some earthquakes!
-You can also visit the [DBOS Cloud Console](https://console.dbos.dev/) to see your app's status and logs.
+You can visit the [DBOS Cloud Console](https://console.dbos.dev/) to see your app's status and logs.
+
+### Running Locally
 
 To run this app locally, you need a Postgres database.
 If you have Docker, you can start one with:
@@ -40,6 +45,5 @@ Then run:
 
 ```shell
 pip install -r requirements.txt
-dbos migrate
 dbos start
 ```
