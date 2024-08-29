09afb31e40161a49e948437e3b38a087e402af52
Max dml <maxdemoulin@gmail.com>
2024-08-29 13:26:39 -0700

    Together.ai demo app (#178)
    Co-authored-by: Peter Kraft <peter.kraft@dbos.dev>

---
+++ python/empyrical_evidences/empyrical/main.py
@@ -0,0 +1,245 @@
+import datetime
+import html
+import re
+from typing import List, Dict
+import requests
+import uuid
+import base64
+from io import BytesIO
+from dbos import DBOS, SetWorkflowUUID
+from fastapi import FastAPI
+from PyPDF2 import PdfReader
+from sqlalchemy.engine import create_engine
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_together import TogetherEmbeddings, ChatTogether
+from langchain_postgres.vectorstores import PGVector
+from langchain_community.vectorstores import DistanceStrategy
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from together import Together
+
+from schema import papers_metadata
+
+app = FastAPI()
+dbos = DBOS(fastapi=app)
+
+embeddings = TogetherEmbeddings(
+    model="togethercomputer/m2-bert-80M-8k-retrieval",
+)
+db_url = dbos.app_db.engine.url
+db_engine = create_engine(db_url)
+vector_store = PGVector(
+    embeddings=embeddings,
+    collection_name="pdf_tokens",
+    connection=db_engine,
+    use_jsonb=True,
+    distance_strategy=DistanceStrategy.COSINE,
+    create_extension=True,
+)
+
+model = ChatTogether(
+    model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+)
+
+#### UPLOAD PAPERS #### 
+@app.get("/uploadPaper")
+def upload_paper(paper_url: str, paper_title: str):
+    paper_id = uuid.uuid4()
+    with SetWorkflowUUID(str(uuid.uuid4())):
+        handle = dbos.start_workflow(upload_paper_workflow, paper_url, paper_title, paper_id)
+    return handle.get_result()
+
+@dbos.workflow()
+def upload_paper_workflow(paper_url: str, paper_title: str, paper_id: uuid.UUID):
+    compensation_actions = []
+
+    # Decode URL from base64. Ensure the string is properly padded and replace + and / with - and _
+    missing_padding = len(paper_url) % 4
+    if missing_padding:
+        paper_url += '=' * (4 - missing_padding)
+    paper_url = paper_url.replace('+', '-')
+    paper_url = paper_url.replace('/', '_')
+    decoded_url = base64.urlsafe_b64decode(paper_url).decode('utf-8')
+
+    # Create a record in the database for the paper
+    record_paper_metadata(paper_title, decoded_url, paper_id)
+    compensation_actions.append(lambda: delete_paper_metadata(paper_id))
+
+    # Download the paper
+    try:
+        paper_blob = download_paper(decoded_url)
+        reader = PdfReader(BytesIO(paper_blob))
+        pages = [page.extract_text() for page in reader.pages]
+    except Exception as e:
+        DBOS.logger.error(f"Failed to download or parse the paper: {e}")
+        for action in compensation_actions:
+            action()
+        return
+
+    # Retrieve the embeddings using together.ai and store them in our vector store
+    try:
+        store_paper_embeddings(pages, paper_id)
+    except Exception as e:
+        DBOS.logger.error(f"Failed to store the embeddings: {e}")
+        for action in compensation_actions:
+            action()
+
+@dbos.transaction()
+def record_paper_metadata(paper_title: str, paper_url: str, paper_id: uuid.UUID):
+    DBOS.sql_session.execute(
+        papers_metadata.insert().values(
+            uuid=paper_id,
+            name=paper_title,
+            url=paper_url,
+        )
+    )
+    DBOS.logger.info(f"Recorded metadata for {paper_title}")
+
+@dbos.transaction()
+def delete_paper_metadata(paper_id: uuid.UUID):
+    DBOS.sql_session.execute(
+        papers_metadata.delete().where(
+            papers_metadata.c.uuid == paper_id
+        )
+    )
+    DBOS.logger.info(f"Deleted metadata for {paper_id}")
+
+@dbos.communicator()
+def download_paper(paper_url: str) -> bytes:
+    DBOS.logger.info(f"Downloading paper from {paper_url}")
+    response = requests.get(paper_url.rstrip())
+    if response.status_code != 200:
+        raise Exception(f"Failed to download paper: {response.status_code}")
+    return response.content
+
+# FIXME: this should be an only-once transaction, but PGVector managers its own connections
+@dbos.communicator()
+def store_paper_embeddings(pages: List[str], paper_id: uuid.UUID):
+    # Create large enough chunks to avoid beeing rate limited by together.ai
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=3000,
+        chunk_overlap=200,
+        length_function=len
+    )
+
+    # Set the paper_id in the Document metadata
+    DBOS.logger.info(f"Chunking {len(pages)} pages")
+    metadatas = [{"id": str(paper_id)} for _ in pages]
+    documents = text_splitter.create_documents(pages, metadatas=metadatas)
+    split_pages = text_splitter.split_documents(documents) 
+
+    # Feed our vector store
+    DBOS.logger.info(f"Storing {len(split_pages)} chunks of length 3000 with overlap 200")
+    vector_store.add_documents(split_pages)
+    DBOS.logger.info("Fed vector store")
+
+#### QUERY PAPERS ####
+search_template = """ <s>[INST]
+    You are the author of this research paper: {context}
+
+    Question: {question}
+   
+    Format your answer as a list of at most 2 words strings. Do not add any additional information. For example:
+    1. Topic 1
+    2. Topic 2
+    3. Topic 3
+    
+    [/INST]
+"""
+search_prompt = ChatPromptTemplate.from_template(search_template)
+
+@app.get("/startSearch")
+def search_paper(paper_id: str):
+    with SetWorkflowUUID(str(uuid.uuid4())):
+        handle = dbos.start_workflow(search_paper_workflow, paper_id)
+    comments = handle.get_result()
+    print(comments)
+    return comments
+
+@dbos.workflow()
+def search_paper_workflow(paper_id: str):
+    # Query the paper for a list of topics
+    retriever = vector_store.as_retriever(
+        filter={"id": paper_id}
+    )
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | search_prompt
+        | model
+        | StrOutputParser()
+    )
+    question = "List the 5 most meaningful topics that represent this paper's contribution."
+    try:
+        topics = chain.invoke(question).split("\n")
+    except Exception as e:
+        DBOS.logger.error(f"Failed to retrieve topics from the paper: {e}")
+        return
+    DBOS.logger.info(topics)
+
+    # Search for hackernews comments on these topics
+    try:
+        results = search_topics(topics)
+    except Exception as e:
+        DBOS.logger.error(f"Failed to search for comments: {e}")
+        return
+
+    # Rank the comments
+    try:
+        relevant_comments = rank_comments(results)
+    except Exception as e:
+        DBOS.logger.error(f"Failed to rank comments: {e}")
+        return
+    return relevant_comments
+
+def search_topics(topics: List[str]) -> Dict[str, List[Dict]]:
+    results = {}
+    for topic in topics:
+        results[topic] = search_hackernews(topic, window_size_hours=730)
+    return results
+
+@dbos.communicator()
+def search_hackernews(topic: str, window_size_hours: int) -> List[Dict[str, str]]:
+    threshold = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=window_size_hours)
+
+    params = {
+        "tags": "comment",
+        "query": topic,
+        "numericFilters": f"created_at_i>{threshold.timestamp()}",
+    }
+    response = requests.get("http://hn.algolia.com/api/v1/search", params).json()
+    DBOS.logger.info(f"Found {len(response['hits'])} comments on topic {topic}")
+
+    hits = []
+    for hit in response["hits"]:
+        # Reformat the comment by unescaping HTML, adding newlines, and removing HTML tags
+        comment = hit["comment_text"]
+        comment = re.sub("<p>", "\n", html.unescape(comment))
+        comment = re.sub("<[^<]+?>", "", comment)
+        url = f"https://news.ycombinator.com/item?id={hit['objectID']}"
+        hits.append({
+            "query_topic": topic,
+            "comment": comment,
+            "url": url,
+            "story_title": hit["story_title"],
+        })
+    return hits
+
+@dbos.communicator()
+def rank_comments(comments: Dict[str, List[Dict]]) -> Dict[str, Dict]:
+    results = {}
+    client = Together()
+    for topic, result in comments.items():
+        if len(result) > 0:
+            response = client.rerank.create(
+                model="Salesforce/Llama-Rank-V1",
+                query="Select the comment most relevant to the topic_query",
+                documents=result,
+                top_n=1
+            )
+            most_relevant_comment = result[response.results[0].index]
+            DBOS.logger.info(f"Most relevant comment for topic {topic}:")
+            DBOS.logger.info(most_relevant_comment['comment'])
+            DBOS.logger.info(most_relevant_comment['url'])
+            results[topic] = most_relevant_comment
+    return results
---
+++ python/empyrical_evidences/empyrical/oldmain.py
@@ -0,0 +1,208 @@
+import datetime
+import os
+import uuid
+from typing import Any, Dict, Optional
+
+from dbos import DBOS, SetWorkflowUUID
+from fastapi import Body, FastAPI
+from fastapi import Request as FastAPIRequest
+from llama_index.core import StorageContext, VectorStoreIndex, set_global_handler
+from llama_index.core.postprocessor import FixedRecencyPostprocessor
+from llama_index.core.prompts import PromptTemplate
+from llama_index.core.schema import NodeRelationship, RelatedNodeInfo, TextNode
+from llama_index.vector_stores.postgres import PGVectorStore
+from slack_bolt import App, BoltRequest
+from slack_bolt.adapter.starlette.handler import to_bolt_request
+from slack_sdk.web import SlackResponse
+
+app = FastAPI()
+dbos = DBOS(fastapi=app)
+
+# Initialize the LlamaIndex components
+PREVIOUS_NODE = None
+# Logs from LlamaIndex will be printed as the `DEBUG` level.
+set_global_handler("simple", logger=DBOS.logger)
+db_url = dbos.app_db.engine.url
+vector_store = PGVectorStore.from_params(
+    database=db_url.database,
+    host=db_url.host,
+    password=db_url.password,
+    port=str(db_url.port),
+    user=db_url.username,
+    embed_dim=1536,  # openai embedding dimension
+    perform_setup=False,  # Already setup through schema migration
+)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+index = VectorStoreIndex([], storage_context=storage_context)
+
+# Initialize the slack app
+slackapp = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+    logger=DBOS.logger,
+)
+
+# Get my own slack ID
+auth_response = slackapp.client.auth_test()
+bot_user_id = auth_response["user_id"]
+
+# Define a post endpoint to handle slack events
+@app.post("/")
+def slack_challenge(request: FastAPIRequest, body: Dict[str, Any] = Body(...)):  # type: ignore
+    if "challenge" in body:
+        DBOS.logger.info("Received challenge")
+        return {"challenge": body["challenge"]}
+    DBOS.logger.debug(f"Incoming event: {body}")
+    return slackapp.dispatch(to_bolt_request(request, request._body))
+
+
+# This handles any incoming message the bot can hear
+@slackapp.message()
+def handle_message(request: BoltRequest) -> None:
+    DBOS.logger.info(f"Received message: {request.body}")
+    event_id = request.body["event_id"]
+    # Start the workflow and respond to Slack without waiting for the workflow to finish
+    # Use the unique event_id to ensure that the workflow runs exactly once for each event
+    with SetWorkflowUUID(event_id):
+        dbos.start_workflow(message_workflow, request.body["event"])
+
+
+# This is the main workflow that processes incoming messages
+@dbos.workflow()
+def message_workflow(message: Dict[str, Any]) -> None:
+    global PREVIOUS_NODE
+    # if message contains a "blocks" key
+    #   then look for a "block" with the type "rich text"
+    #       if you find it
+    #       then look inside that block for an "elements" key
+    #           if you find it
+    #               then examine each one of those for an "elements" key
+    #               if you find it
+    #                   then look inside each "element" for one with type "user"
+    #                   if you find it
+    #                   and if that user matches the bot_user_id
+    #                   then it's a message for the bot
+    if message.get("blocks") is not None:
+        for block in message["blocks"]:
+            if block.get("type") == "rich_text":
+                for rich_text_section in block["elements"]:
+                    for element in rich_text_section["elements"]:
+                        if (
+                            element.get("type") == "user"
+                            and element.get("user_id") == bot_user_id
+                        ):
+                            for element in rich_text_section.get("elements"):
+                                if element.get("type") == "text":
+                                    # the user is asking the bot a question
+                                    query: str = element["text"]
+                                    response = answer_question(query, message)
+                                    post_slack_message(
+                                        str(response),
+                                        message["channel"],
+                                        message.get("thread_ts"),
+                                    )
+                                    return
+    # if it's not a question, it might be a threaded reply
+    # if it's a reply to the bot, we treat it as if it were a question
+    if message.get("thread_ts") is not None:
+        if message.get("parent_user_id") == bot_user_id:
+            query = message["text"]
+            replies = get_slack_replies(message["channel"], message["thread_ts"])
+            response = answer_question(query, message, replies)
+            post_slack_message(str(response), message["channel"], message["thread_ts"])
+            return
+
+    # if it's not any kind of question, we store it in the index along with all relevant metadata
+    user_name = get_user_name(message["user"])
+
+    # format timestamp as YYYY-MM-DD HH:MM:SS
+    dt_object = datetime.datetime.fromtimestamp(float(message["ts"]))
+    formatted_time = dt_object.strftime("%Y-%m-%d %H:%M:%S")
+
+    # format full message
+    text = message["text"]
+
+    # store the message in the index
+    insert_node(text, user_name, formatted_time)
+    DBOS.logger.info(f"Stored message: {text}")
+
+
+# Post a message to a slack channel, optionally in a thread
+@dbos.communicator()
+def post_slack_message(
+    message: str, channel: str, thread_ts: Optional[str] = None
+) -> None:
+    slackapp.client.chat_postMessage(channel=channel, text=message, thread_ts=thread_ts)
+
+
+# Get all the replies in a Slack thread
+@dbos.communicator()
+def get_slack_replies(channel: str, thread_ts: str) -> SlackResponse:
+    return slackapp.client.conversations_replies(channel=channel, ts=thread_ts)
+
+
+# Get a Slack user's username from their user id
+@dbos.communicator()
+def get_user_name(user_id: str) -> str:
+    user_info = slackapp.client.users_info(user=user_id)
+    user_name: str = user_info["user"]["name"]
+    return user_name
+
+
+# Given a query and a slack message, answer the question with LlamaIndex and return the response
+@dbos.communicator()
+def answer_question(
+    query: str, message: Dict[str, Any], replies: Optional[SlackResponse] = None
+) -> Any:
+    who_is_asking = get_user_name(message["user"])
+    replies_stanza = ""
+    if replies is not None:
+        replies_stanza = "In addition to the context above, the question you're about to answer has been discussed in the following chain of replies:\n"
+        for reply in replies["messages"]:
+            replies_stanza += get_user_name(reply["user"]) + ": " + reply["text"] + "\n"
+    template = (
+        "Your context is a series of chat messages. Each one is tagged with 'who:' \n"
+        "indicating who was speaking and 'when:' indicating when they said it, \n"
+        "followed by a line break and then what they said. There can be up to 20 chat messages.\n"
+        "The messages are sorted by recency, so the most recent one is first in the list.\n"
+        "The most recent messages should take precedence over older ones.\n"
+        "---------------------\n"
+        "{context_str}"
+        "\n---------------------\n"
+        "The person who is asking the question is called '"
+        + who_is_asking
+        + "'.\n"
+        + replies_stanza
+        + "\n"
+        "You are a helpful AI assistant who has been listening to everything everyone has been saying. \n"
+        "Given the most relevant chat messages above, please answer this question: {query_str}\n"
+    )
+    qa_template = PromptTemplate(template)
+    postprocessor = FixedRecencyPostprocessor(
+        top_k=20,
+        date_key="when",  # the key in the metadata to find the date
+    )
+    query_engine = index.as_query_engine(
+        similarity_top_k=20, node_postprocessors=[postprocessor]
+    )
+    query_engine.update_prompts({"response_synthesizer:text_qa_template": qa_template})
+    return query_engine.query(query)
+
+
+# Insert a node into LlamaIndex
+@dbos.communicator()
+def insert_node(text: str, user_name: str, formatted_time: str) -> None:
+    global PREVIOUS_NODE
+    # create a node and apply metadata
+    node = TextNode(
+        text=text,
+        id_=str(uuid.uuid4()),
+        metadata={"who": user_name, "when": formatted_time},  # type: ignore
+    )
+    if PREVIOUS_NODE is not None:
+        node.relationships[NodeRelationship.PREVIOUS] = RelatedNodeInfo(
+            node_id=PREVIOUS_NODE.node_id
+        )
+        PREVIOUS_NODE = node
+
+    index.insert_nodes([node])
