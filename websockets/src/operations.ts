bac65b16a2bae88e7d028aa6ae4e5c6b315b6888
manojdbos <manoj.khangaonkar@dbos.dev>
2024-08-29 09:52:04 -0700

    update comment

--- websockets/src/operations.ts
+++ websockets/src/operations.ts
@@ -40,14 +40,15 @@ export class Hello {
     // Upgrade the request to a WebSocket
     const wss = new WebSocket.Server({ noServer: true });
     
-
     wss.handleUpgrade(ctx.koaContext.req, ctx.koaContext.socket, Buffer.alloc(0), (ws) => {
         wss.emit('connection', ws, ctx.koaContext.req);
 
         (async function poll() {
           try {
             while (ws.readyState === WebSocket.OPEN) {
-              const event = Math.floor(Math.random() * 1000000) + 1;; // Await an event
+
+              // create some data. In a real app, this would be an order id or some data that the client needs to know about
+              const event = Math.floor(Math.random() * 1000000) + 1; 
               if (event) {
                 ws.send(JSON.stringify(event)); // Send event to the client
               }
