e5a14353c16ef86b76e94c02a1a960a17db89c42
manojdbos <manoj.khangaonkar@dbos.dev>
2024-08-30 08:30:24 -0700

    review comments

--- websockets/src/operations.ts
+++ websockets/src/operations.ts
@@ -20,23 +20,10 @@ export class Hello {
 
            Run the client wsclient.js or wsclient.py as per instructions in README.md to see websockets in action.<br>
             <br>
-           For the regular starter app and HTTP Visit the route /greeting/:name to be greeted!<br>
-           For example, visit <a href="/greeting/dbos">/greeting/dbos</a>.<br>
-           The counter increments with each page visit.<br>
-           If you visit a new name like <a href="/greeting/alice">/greeting/alice</a>, the counter starts at 1.
            </p></body></html>`;
     return Promise.resolve(readme);
   }
 
-  @GetApi('/greeting/:user') // Serve this function from HTTP GET requests to the /greeting endpoint with 'user' as a path parameter
-  @Transaction()  // Run this function as a database transaction
-  static async helloTransaction(ctxt: TransactionContext<Knex>, @ArgSource(ArgSources.URL) user: string) {
-    // Retrieve and increment the number of times this user has been greeted.
-    const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
-    const { rows } = await ctxt.client.raw(query, [user]) as { rows: dbos_hello[] };
-    const greet_count = rows[0].greet_count;
-    return `Hello, ${user}! You have been greeted ${greet_count} times.\n`;
-  }
 
   @GetApi('/ws') 
   static async sendToClient(ctx: HandlerContext) {
