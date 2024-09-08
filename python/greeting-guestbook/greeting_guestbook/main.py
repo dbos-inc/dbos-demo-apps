44354698a6a5c353a72c0d29e0a373e79d1c6202
Peter Kraft <petereliaskraft@gmail.com>
2024-09-08 16:18:31 -0700

    shorter

--- python/greeting-guestbook/greeting_guestbook/main.py
+++ python/greeting-guestbook/greeting_guestbook/main.py
@@ -13,11 +13,11 @@ DBOS(fastapi=app)
 
 @DBOS.step()
 def sign_guestbook(name: str):
-    url = "https://demo-guest-book.cloud.dbos.dev/record_greeting"
-    headers = {"Content-Type": "application/json"}
-    payload = {"name": name}
-
-    response = requests.post(url, headers=headers, json=payload)
+    response = requests.post(
+        "https://demo-guest-book.cloud.dbos.dev/record_greeting",
+        headers={"Content-Type": "application/json"},
+        json={"name": name},
+    )
     if not response.ok:
         raise Exception(f"Error signing guestbook: {json.dumps(response.json())}")
 
@@ -26,13 +26,7 @@ def sign_guestbook(name: str):
 
 @DBOS.transaction()
 def insert_greeting(name: str) -> str:
-    query = (
-        insert(dbos_hello)
-        .values(name=name, greet_count=1)
-        .on_conflict_do_update(
-            index_elements=["name"], set_={"greet_count": dbos_hello.c.greet_count + 1}
-        )
-    )
+    query = insert(dbos_hello).values(name=name).on_conflict_do_nothing()
     DBOS.sql_session.execute(query)
     DBOS.logger.info(f">>> STEP 2: Greeting to {name} recorded in the database!")
 
