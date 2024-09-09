8593fced39fa492669e7ef0476a562ab18458c38
Peter Kraft <petereliaskraft@gmail.com>
2024-09-09 11:47:32 -0700

    logger

--- python/greeting-guestbook/greeting_guestbook/main.py
+++ python/greeting-guestbook/greeting_guestbook/main.py
@@ -1,3 +1,5 @@
+import logging
+
 import requests
 from dbos import DBOS
 from fastapi import FastAPI
@@ -7,22 +9,24 @@ from .schema import dbos_hello
 app = FastAPI()
 DBOS(fastapi=app)
 
+logging.basicConfig(level=logging.INFO)
+
 
 @DBOS.step()
 def sign_guestbook(name: str):
-    response = requests.post(
+    requests.post(
         "https://demo-guest-book.cloud.dbos.dev/record_greeting",
         headers={"Content-Type": "application/json"},
         json={"name": name},
     )
-    DBOS.logger.info(f">>> STEP 1: Signed the guestbook for {name}")
+    logging.info(f">>> STEP 1: Signed the guestbook for {name}")
 
 
 @DBOS.transaction()
 def insert_greeting(name: str) -> str:
     query = dbos_hello.insert().values(name=name)
     DBOS.sql_session.execute(query)
-    DBOS.logger.info(f">>> STEP 2: Greeting to {name} recorded in the database!")
+    logging.info(f">>> STEP 2: Greeting to {name} recorded in the database!")
 
 
 @app.get("/greeting/{name}")
@@ -30,7 +34,7 @@ def insert_greeting(name: str) -> str:
 def greeting_endpoint(name: str):
     sign_guestbook(name)
     for _ in range(5):
-        DBOS.logger.info("Press Control + C to stop the app...")
+        logging.info("Press Control + C to stop the app...")
         DBOS.sleep(1)
     insert_greeting(name)
     return f"Thank you for being awesome, {name}!"
