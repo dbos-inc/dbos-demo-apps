a924b225ee0db667a08513ee93ae24dbfa1a4940
Qian Li <qian.li@dbos.dev>
2024-09-18 18:37:12 -0700

    Write a story

--- python/ai-quickstart/ai_quickstart/main.py
+++ python/ai-quickstart/ai_quickstart/main.py
@@ -1,7 +1,7 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 from fastapi import FastAPI
-from dbos import DBOS
+from dbos import DBOS, SetWorkflowID
 
 app = FastAPI()
 DBOS(fastapi=app)
@@ -12,6 +12,30 @@ index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
 
 @app.get("/")
-def get_answer():
+@DBOS.step()
+def get_growup():
   response = query_engine.query("What did the author do growing up?")
   return str(response)
+
+@DBOS.step()
+def get_art_school():
+  response = query_engine.query("How did the author start YC?")
+  return str(response)
+
+@DBOS.step()
+def get_yc():
+  response = query_engine.query("What happened after YC?")
+  return str(response)
+
+@DBOS.workflow()
+def story_workflow():
+  res1 = "First, " + get_growup()
+  res2 = " Then, " + get_art_school()
+  res3 = " Finally, " + get_yc()
+  return res1 + res2 + res3
+
+@app.get("/story/{version}")
+@DBOS.workflow()
+def get_story(version: str):
+  with SetWorkflowID(version):
+    return story_workflow()
