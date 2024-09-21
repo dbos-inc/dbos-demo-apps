78cf34440da973566e39e5ddc8bfccc55f4c296e
Qian Li <qian.li@dbos.dev>
2024-09-20 17:15:45 -0700

    refactor

--- python/ai-storyteller/main.py
+++ python/ai-storyteller/main.py
@@ -1,9 +1,7 @@
 # DBOS AI storyteller: A simple FastAPI app that uses LlamaIndex to generate a story and can be deployed to DBOS Cloud.
 
 # First, let's do imports and create FastAPI and DBOS instances.
-from fastapi.responses import HTMLResponse
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-
 from fastapi import FastAPI
 from dbos import DBOS, SetWorkflowID
 
@@ -13,55 +11,57 @@ DBOS(fastapi=app)
 # Then, let's initialize the index and query engine by loading "paul_graham_essay.txt" from the data/ folder.
 documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(documents)
-
 query_engine = index.as_query_engine()
 
 # After that, let's define three steps, each representing a part of the story.
 @DBOS.step()
 def get_growup():
-  response = query_engine.query("What did the author do growing up?")
-  return str(response)
+    response = query_engine.query("What did the author do growing up?")
+    return str(response)
 
 @DBOS.step()
 def get_art_school():
-  response = query_engine.query("How did the author start YC?")
-  return str(response)
+    response = query_engine.query("How did the author start YC?")
+    return str(response)
 
 @DBOS.step()
 def get_yc():
-  response = query_engine.query("What happened after YC?")
-  return str(response)
+    response = query_engine.query("What happened after YC?")
+    return str(response)
 
+# This workflow invokes the above three steps to tell a whole story!
 @DBOS.workflow()
 def story_workflow():
-  res1 = "First, " + get_growup()
-  res2 = " Then, " + get_art_school()
-  res3 = " Finally, " + get_yc()
-  return res1 + res2 + res3
+    res1 = "First, " + get_growup()
+    res2 = " Then, " + get_art_school()
+    res3 = " Finally, " + get_yc()
+    return res1 + res2 + res3
 
-# Finally, let's define a route that generates a story based on the version number.
+# Let's define a route that generates a version of the story.
+# Every time you visit the same version, you get the same story.
 @app.get("/story/{version}")
-@DBOS.workflow()
 def get_story(version: str):
-  with SetWorkflowID(version):
-    return story_workflow()
+    with SetWorkflowID(version):
+        return story_workflow()
 
-# Let's define a route that returns a simple HTML page with instructions on how to generate a story.
+# Finally, let's define a route that returns a simple HTML page with instructions on how to generate a story.
+from fastapi.responses import HTMLResponse
+
 @app.get("/")
 def readme() -> HTMLResponse:
   readme = """
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <title>Welcome to DBOS!</title>
+        <title>DBOS Storyteller</title>
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body class="font-sans text-gray-800 p-6 max-w-2xl mx-auto">
-        <h1 class="text-xl font-semibold mb-4">Welcome to DBOS!</h1>
+        <h1 class="text-xl font-semibold mb-4">DBOS Storyteller</h1>
         <p class="mb-4">
             Visit the route <code class="bg-gray-100 px-1 rounded">/story/{version}</code> to generate a version of the story!<br>
             For example, visit <code class="bg-gray-100 px-1 rounded"><a href="/story/v1" class="text-blue-600 hover:underline">/story/v1</a></code><br>
-            The story remains the same for the same version number.
+            The story remains consistent for the same version number.
             <br>
         </p>
         <p>
