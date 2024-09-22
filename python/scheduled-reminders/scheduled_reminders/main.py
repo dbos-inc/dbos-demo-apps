865a0cd0fab2832b5a7307c5c4692f9168a03566
Peter Kraft <petereliaskraft@gmail.com>
2024-09-22 12:03:34 -0700

    send email

--- python/scheduled-reminders/scheduled_reminders/main.py
+++ python/scheduled-reminders/scheduled_reminders/main.py
@@ -1,11 +1,38 @@
+import os
+
 from dbos import DBOS
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 app = FastAPI()
 DBOS(fastapi=app)
 
+api_key = os.environ.get("SENDGRID_API_KEY", None)
+if api_key is None:
+    raise Exception("Error: SENDGRID_API_KEY is not set")
 
-@app.get("/")
-def endpoint() -> str:
-    return "hello"
+from_email = os.environ.get("SENDGRID_FROM_EMAIL", None)
+if from_email is None:
+    raise Exception("Error: SENDGRID_FROM_EMAIL is not set")
+
+
+# @DBOS.step()
+def send_email(to_email, time):
+    message = Mail(
+        from_email=from_email,
+        to_emails=to_email,
+        subject="DBOS Reminder",
+        html_content=f"This is a reminder from DBOS! It has been {time} since your last reminder.",
+    )
+    email_client = SendGridAPIClient(api_key)
+    response = email_client.send(message)
+
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+
+@app.post("/")
+def endpoint():
+    send_email("peter.kraft@dbos.dev", "one second")
