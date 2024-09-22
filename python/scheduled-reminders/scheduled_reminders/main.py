29139bf1026f7ed63336ec0dac0580ba571f3860
Peter Kraft <petereliaskraft@gmail.com>
2024-09-22 12:21:06 -0700

    more workflow

--- python/scheduled-reminders/scheduled_reminders/main.py
+++ python/scheduled-reminders/scheduled_reminders/main.py
@@ -17,8 +17,8 @@ if from_email is None:
     raise Exception("Error: SENDGRID_FROM_EMAIL is not set")
 
 
-# @DBOS.step()
-def send_email(to_email, time):
+@DBOS.step()
+def send_email(to_email: str, time: str):
     message = Mail(
         from_email=from_email,
         to_emails=to_email,
@@ -26,13 +26,25 @@ def send_email(to_email, time):
         html_content=f"This is a reminder from DBOS! It has been {time} since your last reminder.",
     )
     email_client = SendGridAPIClient(api_key)
-    response = email_client.send(message)
+    email_client.send(message)
+    DBOS.logger.info(f"Email sent to {to_email} at time {time}")
 
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
+
+@DBOS.workflow()
+def reminder_workflow(to_email: str):
+    DBOS.sleep(60)  # Wait for one minute
+    send_email(to_email, time="one minute")
+
+    DBOS.sleep(24 * 60 * 60)  # Wait for one day
+    send_email(to_email, time="one day")
+
+    DBOS.sleep(7 * 24 * 60 * 60)  # Wait for one week
+    send_email(to_email, time="one week")
+
+    DBOS.sleep(30 * 24 * 60 * 60)  # Wait for one month (30 days)
+    send_email(to_email, time="one month")
 
 
 @app.post("/")
 def endpoint():
-    send_email("peter.kraft@dbos.dev", "one second")
+    DBOS.start_workflow(reminder_workflow, "peter.kraft@dbos.dev")
