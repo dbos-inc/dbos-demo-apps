7773f96b10a2f53b530812ab2fdf2232dbfbef21
Peter Kraft <petereliaskraft@gmail.com>
2024-10-08 15:42:59 -0700

    dbos

--- python/chatbot/chatbot/main.py
+++ python/chatbot/chatbot/main.py
@@ -1,10 +1,13 @@
+from dbos import DBOS
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.graph import START, MessagesState, StateGraph
-from dbos import DBOS, get_dbos_database_url
+
+dbos = DBOS()
+DBOS.launch()
 
 model = ChatOpenAI(model="gpt-3.5-turbo")
 
@@ -37,12 +40,14 @@ workflow.add_node("model", call_model)
 
 # Add memory
 # memory = MemorySaver()
-with PostgresSaver.from_conn_string("postgresql://postgres:dbos@localhost:5432/chatbot") as checkpointer:
+db = DBOS.config["database"]
+with PostgresSaver.from_conn_string(
+    f"postgresql://{db["username"]}:{db["password"]}@{db["hostname"]}:{db["port"]}/{db["app_db_name"]}"
+) as checkpointer:
     checkpointer.setup()
     app = workflow.compile(checkpointer=checkpointer)
 
     config = {"configurable": {"thread_id": "abc123"}}
-
 
     query = "Hi! I'm Bob."
 
@@ -55,3 +60,5 @@ with PostgresSaver.from_conn_string("postgresql://postgres:dbos@localhost:5432/c
     input_messages = [HumanMessage(query)]
     output = app.invoke({"messages": input_messages}, config)
     output["messages"][-1].pretty_print()
+
+DBOS.destroy()
