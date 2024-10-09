42863355814dd0abd016a227fbd3c59e53fc020d
Peter Kraft <petereliaskraft@gmail.com>
2024-10-09 15:04:51 -0700

    pirate

--- python/chatbot/chatbot/main.py
+++ python/chatbot/chatbot/main.py
@@ -1,11 +1,15 @@
+import os
+
 from dbos import DBOS
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.graph import START, MessagesState, StateGraph
 from psycopg_pool import ConnectionPool
+from pydantic import BaseModel
 
 app = FastAPI()
 dbos = DBOS(fastapi=app)
@@ -39,16 +43,37 @@ def create_langchain():
     checkpointer = PostgresSaver(pool)
     return workflow.compile(checkpointer=checkpointer)
 
+
 chain = create_langchain()
 
+history = []
 
-@app.post("/")
-def chat_endpoint():
+
+class ChatSchema(BaseModel):
+    query: str
+
+
+@app.post("/chat")
+def chat_endpoint(chat: ChatSchema):
     config = {"configurable": {"thread_id": "default_thread"}}
 
-    query = "Hi! I'm Bob."
+    history.append({"content": chat.query, "isUser": True})
 
-    input_messages = [HumanMessage(query)]
+    input_messages = [HumanMessage(chat.query)]
     output = chain.invoke({"messages": input_messages}, config)
-    output["messages"][-1].pretty_print()  # output contains all messages in state
-    return output["messages"]
+    response = output["messages"][-1].content
+    history.append({"content": response, "isUser": False})
+
+    return {"content": response, "isUser": True}
+
+
+@app.get("/history")
+def history_endpoint():
+    return history
+
+
+@app.get("/")
+def frontend():
+    with open(os.path.join("html", "app.html")) as file:
+        html = file.read()
+    return HTMLResponse(html)
