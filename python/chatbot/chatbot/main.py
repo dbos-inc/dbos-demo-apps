dcf1d1c038347d0799422b42a44ca4dc2f72e1f5
Peter Kraft <petereliaskraft@gmail.com>
2024-10-08 17:17:58 -0700

    chain

--- python/chatbot/chatbot/main.py
+++ python/chatbot/chatbot/main.py
@@ -2,63 +2,57 @@ from dbos import DBOS
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
-from langgraph.checkpoint.memory import MemorySaver
 from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.graph import START, MessagesState, StateGraph
+from psycopg_pool import ConnectionPool
 
 dbos = DBOS()
 DBOS.launch()
 
-model = ChatOpenAI(model="gpt-3.5-turbo")
 
+def create_langchain():
+    model = ChatOpenAI(model="gpt-3.5-turbo")
+    workflow = StateGraph(state_schema=MessagesState)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You talk like a pirate. Answer all questions to the best of your ability.",
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+        ]
+    )
 
-# Define a new graph
-workflow = StateGraph(state_schema=MessagesState)
+    def call_model(state: MessagesState):
+        chain = prompt | model
+        response = chain.invoke(state)
+        return {"messages": response}
 
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You talk like a pirate. Answer all questions to the best of your ability.",
-        ),
-        MessagesPlaceholder(variable_name="messages"),
-    ]
-)
-
-
-# Define the function that calls the model
-def call_model(state: MessagesState):
-    chain = prompt | model
-    response = chain.invoke(state)
-    return {"messages": response}
-
-
-# Define the (single) node in the graph
-workflow.add_edge(START, "model")
-workflow.add_node("model", call_model)
-
-# Add memory
-# memory = MemorySaver()
-db = DBOS.config["database"]
-with PostgresSaver.from_conn_string(
-    f"postgresql://{db["username"]}:{db["password"]}@{db["hostname"]}:{db["port"]}/{db["app_db_name"]}"
-) as checkpointer:
+    workflow.add_edge(START, "model")
+    workflow.add_node("model", call_model)
+    db = DBOS.config["database"]
+    pool = ConnectionPool(
+        f"postgresql://{db["username"]}:{db["password"]}@{db["hostname"]}:{db["port"]}/{db["app_db_name"]}"
+    )
+    checkpointer = PostgresSaver(pool)
     checkpointer.setup()
-    app = workflow.compile(checkpointer=checkpointer)
+    return workflow.compile(checkpointer=checkpointer)
 
-    config = {"configurable": {"thread_id": "abc123"}}
 
-    query = "Hi! I'm Bob."
+chain = create_langchain()
 
-    input_messages = [HumanMessage(query)]
-    output = app.invoke({"messages": input_messages}, config)
-    output["messages"][-1].pretty_print()  # output contains all messages in state
+config = {"configurable": {"thread_id": "default_thread"}}
 
-    query = "What's my name?"
+query = "Hi! I'm Bob."
 
-    input_messages = [HumanMessage(query)]
-    output = app.invoke({"messages": input_messages}, config)
-    output["messages"][-1].pretty_print()
+input_messages = [HumanMessage(query)]
+output = chain.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()  # output contains all messages in state
+
+query = "What's my name?"
+
+input_messages = [HumanMessage(query)]
+output = chain.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
 
 DBOS.destroy()
