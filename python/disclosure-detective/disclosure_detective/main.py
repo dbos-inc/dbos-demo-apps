9381053182a6ff3f0b9a1f5dcb0f8c1d8fccc563
Peter Kraft <petereliaskraft@gmail.com>
2024-10-15 15:47:10 -0700

    docs

--- python/disclosure-detective/disclosure_detective/main.py
+++ python/disclosure-detective/disclosure_detective/main.py
@@ -1,34 +1,17 @@
 import os
 from tempfile import TemporaryDirectory
-from typing import List
 
 import requests
-from dbos import DBOS, load_config
-from llama_index.core import Document, Settings, StorageContext, VectorStoreIndex
+from dbos import load_config
+from llama_index.core import Settings, StorageContext, VectorStoreIndex
 from llama_index.readers.file import PDFReader
 from llama_index.vector_stores.postgres import PGVectorStore
-
-Settings.chunk_size = 512
 
 apple_2024_10K_url = "https://d18rn0p25nwr6d.cloudfront.net/CIK-0000320193/faab4555-c69b-438a-aaf7-e09305f87ca3.pdf"
 
 
-def fetch_and_read_document(
-    document_url,
-) -> List[Document]:
-    with TemporaryDirectory() as temp_dir:
-        temp_file_path = os.path.join(temp_dir, "file.pdf")
-        with open(temp_file_path, "wb") as temp_file:
-            with requests.get(document_url, stream=True) as r:
-                r.raise_for_status()
-                for chunk in r.iter_content(chunk_size=8192):
-                    temp_file.write(chunk)
-            temp_file.seek(0)
-            reader = PDFReader()
-            return reader.load_data(temp_file_path)
-
-
 def construct_index():
+    Settings.chunk_size = 512
     dbos_config = load_config()
     db = dbos_config["database"]
     vector_store = PGVectorStore.from_params(
@@ -44,10 +27,25 @@ def construct_index():
 
 
 index = construct_index()
-apple_doc = fetch_and_read_document(apple_2024_10K_url)
-for doc in apple_doc:
-    index.insert(doc)
 
+
+def index_document(document_url) -> int:
+    with TemporaryDirectory() as temp_dir:
+        temp_file_path = os.path.join(temp_dir, "file.pdf")
+        with open(temp_file_path, "wb") as temp_file:
+            with requests.get(document_url, stream=True) as r:
+                r.raise_for_status()
+                for page in r.iter_content(chunk_size=8192):
+                    temp_file.write(page)
+            temp_file.seek(0)
+            reader = PDFReader()
+            pages = reader.load_data(temp_file_path)
+    for page in pages:
+        index.insert(page)
+    return len(pages)
+
+
+index_document(apple_2024_10K_url)
 
 query_engine = index.as_chat_engine()
 
