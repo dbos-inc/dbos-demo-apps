882563c67ed9e2ffbc7c847a16165db292503b2b
maxdml <maxdml@dbos.dev>
2024-11-19 13:25:15 -0800

    black formatting

--- python/stock-prices/stock_prices/streamlit.py
+++ python/stock-prices/stock_prices/streamlit.py
@@ -8,7 +8,7 @@
 # We separate the Streamlit script from the DBOS ingestion code
 # because Streamlit re-runs the entire script every time it's viewed.
 
-# First, let's do imports and configure Streamlit with a title and some custom CSS.
+# First, let's do imports and configure Streamlit with a title and some custom CSS.
 
 import dbos
 import os
@@ -39,39 +39,38 @@ st.markdown(
 database_url = dbos.get_dbos_database_url()
 engine = create_engine(database_url)
 
+
 # We will use this connection to load stock prices data from the database.
 def load_stocks_data():
-    query = (
-        select(stock_prices)
-        .order_by(desc(stock_prices.c.timestamp))
-        .limit(10000)
-    )
+    query = select(stock_prices).order_by(desc(stock_prices.c.timestamp)).limit(10000)
     df = pd.read_sql(query, engine)
     df["timestamp"] = pd.to_datetime(df["timestamp"])
-    df['rate_of_change'] = df.groupby('stock_symbol')['stock_price'].pct_change() * 100
+    df["rate_of_change"] = df.groupby("stock_symbol")["stock_price"].pct_change() * 100
     return df
+
+
 stocks_prices_df = load_stocks_data()
 
 # Create a sidebar with a dropdown filter for stock symbols
 with st.sidebar:
     st.header("Filters")
-    stock_symbols = ["All"] + stocks_prices_df['stock_symbol'].unique().tolist()
+    stock_symbols = ["All"] + stocks_prices_df["stock_symbol"].unique().tolist()
     stock_symbol_filter = st.selectbox(
-        "Select Stock Symbol",
-        options=stock_symbols,
-        index=0 # Select "All" by default
+        "Select Stock Symbol", options=stock_symbols, index=0  # Select "All" by default
     )
     st.header("Display")
     display_mode = st.selectbox(
         "Select Display Mode",
         options=["Stock Prices", "Rate of Change"],
-        index=1 # Select "Rate of Change" by default
+        index=1,  # Select "Rate of Change" by default
     )
 
 if stock_symbol_filter == "All":
     filtered_df = stocks_prices_df
 else:
-    filtered_df = stocks_prices_df[stocks_prices_df['stock_symbol'] == stock_symbol_filter]
+    filtered_df = stocks_prices_df[
+        stocks_prices_df["stock_symbol"] == stock_symbol_filter
+    ]
 if display_mode == "Stock Prices":
     y_label = "Stock Price"
     y_column = "stock_price"
@@ -81,8 +80,12 @@ else:
 
 # Now let's group the stock prices by stock symbol and plot them using Plotly Express.
 fig = px.line(
-    filtered_df, x='timestamp', y=y_column, color='stock_symbol',
-    markers=True, title='Stocks Values Over Time'
+    filtered_df,
+    x="timestamp",
+    y=y_column,
+    color="stock_symbol",
+    markers=True,
+    title="Stocks Values Over Time",
 )
 # Set the x-axis title
 fig.update_xaxes(title_text="Timestamp")
@@ -92,12 +95,14 @@ fig.update_yaxes(title_text=y_label)
 # Display the plot in Streamlit
 st.plotly_chart(fig)
 
+
 # Now, let's add a table to manage alerts
 # First let's load the alerts data from the database
 def load_alerts_data():
     query = select(alerts)
     df = pd.read_sql(query, engine)
-    return df.drop(columns=['phone_number'])
+    return df.drop(columns=["phone_number"])
+
 
 alerts_df = load_alerts_data()
 
@@ -108,11 +113,11 @@ st.header("Manage SMS Alerts")
 alert_stock_symbol = st.selectbox(
     "Select Stock Symbol",
     key="alert_stock_symbol",
-    options=stocks_prices_df['stock_symbol'].unique().tolist(),
-    index=0 # Select the first stock symbol by default
+    options=stocks_prices_df["stock_symbol"].unique().tolist(),
+    index=0,  # Select the first stock symbol by default
 )
 price_threshold = st.text_input("Price Threshold")
-phone_number = os.environ.get('MY_PHONE_NUMBER')
+phone_number = os.environ.get("MY_PHONE_NUMBER")
 
 if st.button("Create Alert"):
     if alert_stock_symbol and price_threshold and phone_number:
@@ -131,8 +136,8 @@ if st.button("Create Alert"):
 
 alert_to_delete = st.selectbox(
     "Select Alert to delete",
-    options=alerts_df['stock_symbol'].tolist(),
-    index=0 # Select the first alert by default
+    options=alerts_df["stock_symbol"].tolist(),
+    index=0,  # Select the first alert by default
 )
 
 if st.button("Delete an alert"):
