b450c61cc590ce8b4c5d2af0edbe4b2e140b1973
maxdml <maxdml@dbos.dev>
2024-11-19 13:25:15 -0800

    black formatting

--- python/stock-prices/stock_prices/main.py
+++ python/stock-prices/stock_prices/main.py
@@ -1,7 +1,7 @@
 # Welcome to DBOS!
 
 # This is a DBOS port of the stock prices monitoring app showcased in
-# https://python.plainenglish.io/building-and-deploying-a-stock-price-monitoring-system-with-aws-lambda-rds-postgresql-and-4abef0b3968a
+# https://python.plainenglish.io/building-and-deploying-a-stock-price-monitoring-system-with-aws-lambda-rds-postgresql-and-4abef0b3968a
 
 # It periodically fetches stock prices from Yahoo Finance and stores them in a Postgres database.
 
@@ -18,6 +18,7 @@ import threading
 
 DBOS()
 
+
 # Then let's write a function that fetches stock prices from Yahoo Finance.
 # We annotate this function with `@DBOS.step` so we can call it from a durable workflow later on.
 @DBOS.step()
@@ -27,40 +28,53 @@ def fetch_stock_price(symbol):
     if data.empty:
         raise ValueError("No stock data found for the symbol.")
     DBOS.logger.info(f"Stock price for {symbol} is {data['Close'].iloc[0]}")
-    return data['Close'].iloc[0]
+    return data["Close"].iloc[0]
+
 
 # Next, let's write a function that saves stock prices to a Postgres database.
 @DBOS.transaction()
 def save_to_db(symbol, price):
-    DBOS.sql_session.execute(stock_prices.insert().values(stock_symbol=symbol, stock_price=price))
+    DBOS.sql_session.execute(
+        stock_prices.insert().values(stock_symbol=symbol, stock_price=price)
+    )
+
 
 # Now, let's write a function that will send an SMS alert.
 # We will use Twilio for this. You can sign up for a free Twilio account at https://www.twilio.com/try-twilio
 # We define use environment variables dbos-config.yaml to store our Twilio account SID, auth token, phone number, and our own phone number.
-twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
-twilio_auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
-twilio_phone_number = os.environ.get('TWILIO_PHONE_NUMBER')
+twilio_account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+twilio_auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+twilio_phone_number = os.environ.get("TWILIO_PHONE_NUMBER")
+
+
 @DBOS.step()
 def send_sms_alert(symbol, price, to_phone_number):
     client = Client(twilio_account_sid, twilio_auth_token)
     message = client.messages.create(
         body=f"{symbol} stock price is {price}.",
         from_=twilio_phone_number,
-        to=to_phone_number
+        to=to_phone_number,
     )
     DBOS.logger.info(f"SMS sent: {message.sid}")
+
 
 # Let's write a small function to retrieve alerts from the database
 @DBOS.transaction()
 def fetch_alerts():
     query = alerts.select()
-    return {alert.stock_symbol:alert for alert in DBOS.sql_session.execute(query).fetchall()}
+    return {
+        alert.stock_symbol: alert
+        for alert in DBOS.sql_session.execute(query).fetchall()
+    }
+
 
 # Then, let's write a scheduled job that fetches stock prices for a list of symbols every minute.
 # The @DBOS.scheduled() decorator tells DBOS to run this function on a cron schedule.
 # The @DBOS.workflow() decorator tells DBOS to run this function as a reliable workflow, so it runs exactly-once per minute.
-symbols = ['AAPL', 'GOOGL', 'AMZN', 'MSFT', 'TSLA', 'NVDA']
-@DBOS.scheduled('* * * * *')
+symbols = ["AAPL", "GOOGL", "AMZN", "MSFT", "TSLA", "NVDA"]
+
+
+@DBOS.scheduled("* * * * *")
 @DBOS.workflow()
 def fetch_stock_prices_workflow(scheduled_time: datetime, actual_time: datetime):
     # Fetch registered alerts
@@ -73,6 +87,7 @@ def fetch_stock_prices_workflow(scheduled_time: datetime, actual_time: datetime)
         if registered_alerts and symbol in registered_alerts:
             if price > registered_alerts[symbol].price_threshold:
                 send_sms_alert(symbol, price, registered_alerts[symbol].phone_number)
+
 
 # Finally, in our main function, let's launch DBOS, then sleep the main thread forever
 # while the background threads run.
--- python/stock-prices/stock_prices/schema.py
+++ python/stock-prices/stock_prices/schema.py
@@ -1,4 +1,13 @@
-from sqlalchemy import Column, MetaData, String, Table, Numeric, TIMESTAMP, func, UniqueConstraint
+from sqlalchemy import (
+    Column,
+    MetaData,
+    String,
+    Table,
+    Numeric,
+    TIMESTAMP,
+    func,
+    UniqueConstraint,
+)
 
 metadata = MetaData()
 
@@ -6,16 +15,20 @@ stock_prices = Table(
     "stock_prices",
     metadata,
     Column("stock_symbol", String(10), nullable=False),
-    Column("stock_price", Numeric(10,2), nullable=False),
-    Column("timestamp", TIMESTAMP, server_default=func.current_timestamp(), nullable=False),
+    Column("stock_price", Numeric(10, 2), nullable=False),
+    Column(
+        "timestamp", TIMESTAMP, server_default=func.current_timestamp(), nullable=False
+    ),
 )
 
 alerts = Table(
     "alerts",
     metadata,
     Column("stock_symbol", String(10), nullable=False),
-    Column("price_threshold", Numeric(10,2), nullable=False),
+    Column("price_threshold", Numeric(10, 2), nullable=False),
     Column("phone_number", String(20), nullable=False),
-    Column("created_at", TIMESTAMP, server_default=func.current_timestamp(), nullable=False),
-    UniqueConstraint("stock_symbol", name="unique_stock_symbol")
+    Column(
+        "created_at", TIMESTAMP, server_default=func.current_timestamp(), nullable=False
+    ),
+    UniqueConstraint("stock_symbol", name="unique_stock_symbol"),
 )
