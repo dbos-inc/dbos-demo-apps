70a6d020f6dc5d776d88a85ab2a630f5f6854337
Peter Kraft <petereliaskraft@gmail.com>
2024-12-19 11:28:29 -0800

    start

--- typescript/greeting-guestbook/src/operations.ts
+++ typescript/greeting-guestbook/src/operations.ts
@@ -1,71 +1,81 @@
-import {
-    TransactionContext, Transaction, StepContext, Step,
-    WorkflowContext, Workflow, GetApi, HandlerContext, DBOSResponseError
-} from "@dbos-inc/dbos-sdk";
-import { Knex } from "knex";
+// Welcome to DBOS!
 
-interface GreetingRecord {
+// This is a sample "Hello" app built with DBOS, Express.js, and Knex.
+// It greets visitors and keeps track of how many times each visitor has been greeted.
+
+// First let's import express and DBOS
+import express from "express";
+import { DBOS } from "@dbos-inc/dbos-sdk";
+
+// Then, let's declare a type representing the "dbos_hello" database table
+export interface dbos_hello {
   name: string;
-  note: string;
+  greet_count: number;
 }
 
-export class Greetings {
-  @GetApi('/') // Serve a quick readme for the app
-  static async readme(_ctxt: HandlerContext) {
-    const readme = '<html><body><p>' +
-      'Welcome! Visit the route /greeting/:name to be greeted!<br>' +
-      'For example, visit <a href="/greeting/dbos">/greeting/dbos</a>.<br>' +
-      '</p></body></html>';
-    return Promise.resolve(readme);
+// Now let's define a class with some static functions.
+// DBOS uses TypeScript decorators to automatically make your functions reliable, so they need to be static.
+export class Hello {
+  // This function greets a user and increments the greet count in the database.
+  // The @DBOS.transaction() decorator ensures that this function runs as a database transaction.
+  @DBOS.transaction()
+  static async helloTransaction(user: string) {
+    const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
+    const { rows } = (await DBOS.knexClient.raw(query, [user])) as { rows: dbos_hello[] };
+    const greet_count = rows[0].greet_count;
+    const greeting = `Hello, ${user}! You have been greeted ${greet_count} times.`;
+    return Hello.makeHTML(greeting);
   }
 
-  @Transaction({readOnly: true})
-  @GetApi('/greetings')
-  static async allGreetings(ctxt: TransactionContext<Knex>) {
-    return await ctxt.client('greetings').select('*') as GreetingRecord[];
+  // Finally, we will declare helper functions to serve static HTML to user.s
+
+  static async readme() {
+    const message = Hello.makeHTML(
+      `Visit the route <code class="bg-gray-100 px-1 rounded">/greeting/{name}</code> to be greeted!<br>
+      For example, visit <code class="bg-gray-100 px-1 rounded"><a href="/greeting/Mike" class="text-blue-600 hover:underline">/greeting/Mike</a></code><br>
+      The counter increments with each page visit.`
+    );
+    return Promise.resolve(message);
   }
 
-  @Step()
-  static async SignGuestbook(ctxt: StepContext, name: string) {
-    const key = process.env.GUESTBOOK_KEY;  //set in dbos-config.yaml
-    if (!key || key.length !== 36) {
-      throw new DBOSResponseError("Please set the guestbook key in dbos-config.yaml", 401);
-    }
-    const response = await fetch('https://demo-guestbook.cloud.dbos.dev/record_greeting', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({ 'key':  key, 'name': name})
-    });
-    const responseStr = JSON.stringify(await response.json());
-    if (!response.ok) {
-      throw new DBOSResponseError(responseStr);
-    }
-    ctxt.logger.info(`>>> STEP 1: Signed the Guestbook: ${responseStr}`);
-  }
-
-  @Transaction()
-  static async InsertGreeting(ctxt: TransactionContext<Knex>, gr: GreetingRecord) {
-    await ctxt.client('greetings').insert(gr);
-    ctxt.logger.info(`>>> STEP 2: Greeting to ${gr.name} recorded in the database!`);
-  }
-
-  @Workflow()
-  static async GreetingWorkflow(ctxt: WorkflowContext, friend: string, noteContent: string) {
-      await ctxt.invoke(Greetings).SignGuestbook(friend);
-      for (let i = 0; i < 5; i++) {
-          ctxt.logger.info("Press Control + C to stop the app...");
-          await ctxt.sleepms(1000);
-      }
-      await ctxt.invoke(Greetings).InsertGreeting(
-        { name: friend, note: noteContent }
-      );
-  }
-
-  @GetApi('/greeting/:friend')
-  static async Greeting(ctxt: HandlerContext, friend: string) {
-    const noteContent = `Thank you for being awesome, ${friend}!`;
-    await ctxt.startWorkflow(Greetings).GreetingWorkflow(friend, noteContent);
-    return Promise.resolve(noteContent);
+  // A helper function to create HTML pages with some styling
+  static makeHTML(message: string) {
+    const page = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <title>DBOS Template App</title>
+          <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body class="font-sans text-gray-800 p-6 max-w-2xl mx-auto">
+          <h1 class="text-3xl font-semibold mb-4">Welcome to DBOS!</h1>
+          <p class="mt-8 mb-8">` + message + `</p>
+          <p class="mb-2">
+              To learn how to run this app yourself, visit our
+              <a href="https://docs.dbos.dev/quickstart?language=typescript" class="text-blue-600 hover:underline">Quickstart</a>.
+          </p><p class="mb-2">
+              Then, to learn how to build crashproof apps, continue to our
+              <a href="https://docs.dbos.dev/typescript/programming-guide" class="text-blue-600 hover:underline">Programming Guide</a>.<br>
+          </p>
+      </body>
+      </html>`;
+    return page;
   }
 }
 
+// Now, let's create an Express app and define some routes.
+export const app = express();
+// Parse JSON payloads and make it available to req.body
+app.use(express.json());
+
+// We'll serve the README at the root of the app
+app.get("/", async (_, res) => {
+  res.send(await Hello.readme());
+});
+
+// Serve this function from HTTP GET requests at the /greeting endpoint with 'user' as a path parameter
+// The handler will in turn call a reliable DBOS operation (helloTransaction) to greet the user
+app.get("/greeting/:user", async (req, res) => {
+  const { user } = req.params;
+  res.send(await Hello.helloTransaction(user));
+});
