789ef16b84cba483a1ebcb16fc67d842dd1797d7
Peter Kraft <petereliaskraft@gmail.com>
2024-12-19 13:57:43 -0800

    dbos

--- typescript/greeting-guestbook/src/operations.ts
+++ typescript/greeting-guestbook/src/operations.ts
@@ -1,8 +1,10 @@
+import { DBOS } from '@dbos-inc/dbos-sdk';
 import express, { Request, Response } from 'express';
 import knex from 'knex';
 const knexConfig = require('../knexfile');
 
 export class Guestbook {
+  @DBOS.step()
   static async signGuestbook(name: string): Promise<void> {
     await fetch("https://demo-guestbook.cloud.dbos.dev/record_greeting", {
       method: 'POST',
@@ -16,6 +18,7 @@ export class Guestbook {
 
   static db = knex(knexConfig);
 
+  @DBOS.step()
   static async insertGreeting(name: string): Promise<void> {
     try {
       await Guestbook.db('dbos_greetings').insert({ greeting_name: name });
@@ -25,8 +28,13 @@ export class Guestbook {
     }
   }
 
+  @DBOS.workflow()
   static async greetingEndpoint(name: string): Promise<string> {
     await Guestbook.signGuestbook(name);
+    for (let i = 0; i < 5; i++) {
+      console.log("Press Control + C to stop the app...");
+      await DBOS.sleep(1000);
+  }
     await Guestbook.insertGreeting(name);
     return `Thank you for being awesome, ${name}!`;
   }
