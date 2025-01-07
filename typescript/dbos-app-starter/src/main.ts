fd0d8e184b1f6be6078287d412356dea715a9c99
Qian Li <qian.li@dbos.dev>
2025-01-06 20:04:44 -0800

    Fix logging and formatting

--- typescript/dbos-app-starter/src/main.ts
+++ typescript/dbos-app-starter/src/main.ts
@@ -1,6 +1,6 @@
-import { DBOS } from '@dbos-inc/dbos-sdk';
-import express, { Request, Response } from 'express';
-import path from 'path';
+import { DBOS } from "@dbos-inc/dbos-sdk";
+import express from "express";
+import path from "path";
 
 // Welcome to DBOS!
 // This is a template application built with DBOS and Express.
@@ -12,7 +12,6 @@ app.use(express.json());
 const stepsEvent = "steps_event";
 
 export class MyApp {
-
   // This workflow simulates a background task with N steps.
 
   // DBOS workflows are resilient to any failure--if your program is crashed,
@@ -28,45 +27,46 @@ export class MyApp {
 
   @DBOS.step()
   static async backgroundTaskStep(step: number): Promise<void> {
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     DBOS.logger.info(`Completed step ${step}!`);
   }
-
 }
 
 // This endpoint uses DBOS to idempotently launch a crashproof background task with N steps.
-app.get('/background/:taskid/:steps', async (req: Request, res: Response): Promise<void> => {
-  const { taskid, steps } = req.params;
-  DBOS.startWorkflow(MyApp, {workflowID: taskid}).launchTaskWithSteps(Number(steps));
-  res.send('Task launched!');
-});
+app.get("/background/:taskid/:steps", async (req, res) => {
+    const { taskid, steps } = req.params;
+    await DBOS.startWorkflow(MyApp, { workflowID: taskid }).launchTaskWithSteps(Number(steps));
+    res.send("Task launched!");
+  }
+);
 
 // This endpoint retrieves the status of a specific background task.
-app.get('/last_step/:taskid', async (req: Request, res: Response): Promise<void> => {
-  const { taskid } = req.params;
-  const step = await DBOS.getEvent(taskid, stepsEvent);
-  res.send(String(step !== null ? step : 0));
-});
+app.get("/last_step/:taskid", async (req, res) => {
+    const { taskid } = req.params;
+    const step = await DBOS.getEvent(taskid, stepsEvent);
+    res.send(String(step !== null ? step : 0));
+  }
+);
 
 // This endpoint crashes the application. For demonstration purposes only :)
-app.post('/crash', (_, _res): void => {
+app.post("/crash", (_, _res): void => {
   process.exit(1);
 });
 
 // This code serves the HTML readme from the root path.
-app.get('/', (_, res) => {
-  const filePath = path.resolve(__dirname, '..', 'html', 'app.html');
+app.get("/", (_, res) => {
+  const filePath = path.resolve(__dirname, "..", "html", "app.html");
   res.sendFile(filePath, (err) => {
     if (err) {
       console.log(err);
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     }
   });
-})
+});
 
 // Launch DBOS and start the Express.js server
 async function main() {
-  await DBOS.launch({expressApp: app});
+  await DBOS.launch({ expressApp: app });
   const PORT = DBOS.runtimeConfig?.port || 3000;
   app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
