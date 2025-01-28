8c74c36d842e510b83ac2d6099fe5f47e9cfe46b
Chuck B <chuck.bear@dbos.dev>
2025-01-28 15:55:16 -0500

    Add nodemon

--- typescript/dbos-nextjs-starter/eslint.config.mjs
+++ typescript/dbos-nextjs-starter/eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    }
+  },
 ];
 
 export default eslintConfig;
