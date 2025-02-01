27202ac21cc0785f03ec55d3060d0b1d70d20823
Peter Kraft <petereliaskraft@gmail.com>
2025-01-31 16:43:51 -0800

    favicon

--- python/dbos-toolbox/main.py
+++ python/dbos-toolbox/main.py
@@ -103,6 +103,7 @@ async def read_root():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="https://dbos-blog-posts.s3.us-west-1.amazonaws.com/live-demo/favicon.ico" type="image/x-icon">
         <script src="https://cdn.tailwindcss.com"></script>
         <title>DBOS Toolbox</title>
     </head>
