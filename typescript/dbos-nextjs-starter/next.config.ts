bc98e1ae0b9758a569df8a2ad5ea7c3b867a68db
Chuck B <chuck.bear@dbos.dev>
2025-03-03 17:04:46 -0500

    No need for this

--- typescript/dbos-nextjs-starter/next.config.ts
+++ typescript/dbos-nextjs-starter/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer, dev }) => {
-    // Treat @dbos-inc/dbos-sdk as an external package for client builds
+  webpack: (config, { isServer, dev:_dev }) => {
+    // Treat @dbos-inc/dbos-sdk and code using it as an external package for builds
     if (isServer) {
       config.externals = [
         ...config.externals,
@@ -12,24 +12,6 @@ const nextConfig: NextConfig = {
         },
         /^@dbos\/.+$/, // ✅ Treat ALL `@dbos/*` imports as external
       ];
-    }
-
-    if (isServer || dev) {
-      //If the minimizer is causing problems with class / function names,
-      //  consider the following.
-      const TerserPlugin = require('terser-webpack-plugin');
-
-      config.optimization.minimizer = [
-        new TerserPlugin({
-          terserOptions: {
-            keep_classnames: true, // Preserve class names
-            keep_fnames: true,    // Preserve function names
-          },
-        }),
-      ];
-
-      // Or, more draconian:
-      // config.optimization.minimize = false;
     }
 
     return config;
