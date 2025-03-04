ad1194815b79cdaf456abdd9efc9806bfbfbf97c
Chuck B <chuck.bear@dbos.dev>
2025-03-03 20:41:40 -0500

    Cleanup

--- typescript/dbos-nextjs-starter/next.config.ts
+++ typescript/dbos-nextjs-starter/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer, dev:_dev }) => {
+  webpack: (config, { isServer, dev: _dev }) => {
     // Treat @dbos-inc/dbos-sdk and code using it as an external package for builds
     if (isServer) {
       config.externals = [
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
         {
           "@dbos-inc/dbos-sdk": "commonjs @dbos-inc/dbos-sdk",
         },
-        /^@dbos\/.+$/, // ✅ Treat ALL `@dbos/*` imports as external
+        /^@dbos\/.+$/, // Treat ALL `@dbos/*` imports as external
       ];
     }
 
