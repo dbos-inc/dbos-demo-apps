c678834c044d7e2674ab9998752c5b85bc129ea5
maxdml <maxdml@dbos.dev>
2025-03-24 14:22:12 -0700

    add jest config

---
+++ typescript/alert-center/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRegex: '((\\.|/)(test|spec))\\.ts?$',
+    testPathIgnorePatterns: ['examples/*', 'packages/*'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    modulePaths: ['./'],
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts'],
+  };
+  
