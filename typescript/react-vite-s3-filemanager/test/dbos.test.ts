f19360c499f3f6c5c6ce6cbe5f3280e5500a737d
Chuck B <chuck.bear@dbos.dev>
2025-03-28 14:15:49 -0400

    Cleanup

--- typescript/react-vite-s3-filemanager/test/dbos.test.ts
+++ typescript/react-vite-s3-filemanager/test/dbos.test.ts
@@ -2,9 +2,9 @@ import { beforeEach, afterEach, describe, expect, it } from "vitest";
 import { DBOS, parseConfigFile } from "@dbos-inc/dbos-sdk";
 import { MyWorkflow } from "../dbos/operations";
 
-describe("math functions", () => {
+describe("dbos functions", () => {
   beforeEach(async () => {
-    const [config, _rtconfig] = parseConfigFile();
+    const [config] = parseConfigFile();
     DBOS.setConfig(config); // Could do something else here, since this is a test
     await DBOS.launch();
   });
@@ -13,7 +13,7 @@ describe("math functions", () => {
     await DBOS.shutdown();
   });
 
-  it("adds two numbers", async () => {
+  it("runs-dbos-code", async () => {
     await MyWorkflow.backgroundTask(4);
     expect(1).toBe(1);
   });
--- typescript/react-vite-s3-filemanager/test/utils.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { add } from "../dbos/utils";
-
-describe("math functions", () => {
-  it("adds two numbers", () => {
-    expect(add(2, 3)).toBe(5);
-  });
-});
