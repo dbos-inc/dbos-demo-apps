eab53cbe92e1f279901b8b05554a8089d6f747a8
Chuck B <chuck.bear@dbos.dev>
2025-03-31 07:24:08 -0400

    Update to 19

--- typescript/bank/bank-frontend/src/app/owner-name-dialog/owner-name-dialog.component.ts
+++ typescript/bank/bank-frontend/src/app/owner-name-dialog/owner-name-dialog.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-owner-name-dialog',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './owner-name-dialog.component.html',
-  styleUrl: './owner-name-dialog.component.css'
+  styleUrls: ['./owner-name-dialog.component.css']
 })
 export class OwnerNameDialogComponent {
   ownerName: string = '';
