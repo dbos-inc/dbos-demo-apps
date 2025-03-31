c7af69564d0e64886840d0c536f33c13ed706536
chuck-dbos <134347445+chuck-dbos@users.noreply.github.com>
2025-03-31 12:30:12 -0400

    Fix demo app libs (#293)
    The change to angular 19 was exciting, everything else should be
    routine.

--- typescript/bank/bank-frontend/src/app/home.component.ts
+++ typescript/bank/bank-frontend/src/app/home.component.ts
@@ -1,47 +1,63 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AppService } from './app.service';
-import  { environment } from "../environments/environment";
+import { environment } from "../environments/environment";
+import { BankComponent } from './bank.component';
 
 @Component({
   selector: 'home-header',
+  standalone: true,
+  imports: [CommonModule, BankComponent],
   providers: [AppService],
   template: `
-<div class="container" xmlns="http://www.w3.org/1999/html">
-    <div *ngIf="!isLoggedIn" class="jumbotron">
-      <h1>Welcome to Bank of DBOS!</h1>
-      <button class="btn btn-primary" (click)="login()" type="submit">Login</button>
-    </div>
-    <div *ngIf="isLoggedIn">
-      <div class="jumbotron">
-        <h1>Welcome {{getUser()}}!</h1>
-        <div class="btn-group">
-          <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Select a Bank Branch / Action
-          </button>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" *ngFor="let host of _service.bankHosts; index as i"role="button" (click)="selectBank(i+1, host)" href="#">Branch-{{i+1}}: {{host}}</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" (click)="refreshLogin()" href="#" role="button">Refresh</a>
-            <a class="dropdown-item" (click)="logout()" href="#" role="button">Logout</a>
+    <div class="container" xmlns="http://www.w3.org/1999/html">
+      <div *ngIf="!isLoggedIn" class="jumbotron">
+        <h1>Welcome to Bank of DBOS!</h1>
+        <button class="btn btn-primary" (click)="login()" type="submit">Login</button>
+      </div>
+      <div *ngIf="isLoggedIn">
+        <div class="jumbotron">
+          <h1>Welcome {{ getUser() }}!</h1>
+          <div class="btn-group">
+            <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Select a Bank Branch / Action
+            </button>
+            <div class="dropdown-menu">
+              <a
+                class="dropdown-item"
+                *ngFor="let host of _service.bankHosts; index as i"
+                role="button"
+                (click)="selectBank(i + 1, host)"
+                href="#"
+              >
+                Branch-{{ i + 1 }}: {{ host }}
+              </a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" (click)="refreshLogin()" href="#" role="button">Refresh</a>
+              <a class="dropdown-item" (click)="logout()" href="#" role="button">Logout</a>
+            </div>
           </div>
         </div>
+        <bank-details
+          *ngIf="selectedBankId && selectBankHost"
+          [bankUrl]="selectBankHost"
+          [bankId]="selectedBankId"
+        ></bank-details>
       </div>
-      <bank-details *ngIf="selectedBankId && selectBankHost"  [bankUrl]="selectBankHost" [bankId]="selectedBankId"></bank-details>
     </div>
-</div>`
+  `
 })
-
 export class HomeComponent {
   public isLoggedIn = false;
   selectedBankId: number = 1;
   selectBankHost: string = this._service.bankHosts[0];
 
-  constructor(public _service:AppService){}
+  constructor(public _service: AppService) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.isLoggedIn = this._service.checkCredentials();
     const i = window.location.href.indexOf('code');
-    if (!this.isLoggedIn && i != -1){
+    if (!this.isLoggedIn && i != -1) {
       this._service.retrieveToken(window.location.href.substring(i + 5));
     }
   }
@@ -53,7 +69,7 @@ export class HomeComponent {
 
   login() {
     window.location.href = environment.authUrl + '/auth?response_type=code&&scope=openid&client_id=' +
-          this._service.clientId + '&redirect_uri='+ this._service.redirectUri;
+          this._service.clientId + '&redirect_uri=' + this._service.redirectUri;
   }
 
   logout() {
@@ -68,6 +84,6 @@ export class HomeComponent {
   refreshLogin() {
     this._service.logoutOnError();
     window.location.href = environment.authUrl + '/auth?response_type=code&&scope=openid&client_id=' +
-        this._service.clientId + '&redirect_uri='+ this._service.redirectUri;
+        this._service.clientId + '&redirect_uri=' + this._service.redirectUri;
   }
 }
