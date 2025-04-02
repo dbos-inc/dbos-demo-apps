17c9fa73d9a29b68b9ec903da9585943dd514a21
Chuck B <chuck.bear@dbos.dev>
2025-04-02 10:05:50 -0400

    Remove YKY

--- typescript/yky-social/yky/app/dogetmediakey/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from 'next/server';
-
-import { placeApiRequest } from '@/app/components/backend';
-
-export async function GET(request: NextRequest) {
-  return await placeApiRequest(request, async (api, req, hdrs) => {
-    return await api.getProfilePhoto({}, hdrs);
-  })
-}
