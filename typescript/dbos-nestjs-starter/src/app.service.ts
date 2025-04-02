aa2788b1a69a1c0e2add37e63d63bf8ca634cec1
Chuck B <chuck.bear@dbos.dev>
2025-04-02 11:44:09 -0400

    Remove InitContext

--- typescript/dbos-nestjs-starter/src/app.service.ts
+++ typescript/dbos-nestjs-starter/src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { ConfiguredInstance, DBOS, InitContext } from "@dbos-inc/dbos-sdk";
+import { ConfiguredInstance, DBOS } from "@dbos-inc/dbos-sdk";
 import {
   uniqueNamesGenerator,
   adjectives,
@@ -18,7 +18,7 @@ export class AppService extends ConfiguredInstance {
     super(name);
   }
 
-  async initialize(ctx: InitContext): Promise<void> {
+  override async initialize(): Promise<void> {
     DBOS.logger.info(`Initializing DBOS provider ${this.name}`);
   }
 
