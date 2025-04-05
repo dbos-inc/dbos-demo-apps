248feb0d7b5bdaf15f053c9090ae4fbe85d65e54
maxdml <maxdml@dbos.dev>
2025-04-04 18:43:41 -0700

    functional DBOS-enabled django app

---
+++ python/dbos-django-starter/README.md
@@ -0,0 +1,8 @@
+python manage.py sqlmigrate polls 0001- This guide uses a Django application from the Django quickstart guide (https://docs.djangoproject.com/en/5.2/intro/tutorial01/)
+- It assumes you have created a postgres database named "djangodbos"
+
+
+python manage.py runserver --noreload
+
+http://127.0.0.1:8000/polls/callWorkflow/a/b
+
---
+++ python/dbos-django-starter/polls/apps.py
@@ -0,0 +1,15 @@
+from django.apps import AppConfig
+from dbos import DBOS
+
+class PollsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'polls'
+
+    def ready(self):
+        dbos_config = {
+            "name": "django-app",
+            "log_level": "DEBUG",
+        }
+        dbos = DBOS(config=dbos_config)
+        dbos.launch()
+        return super().ready()
