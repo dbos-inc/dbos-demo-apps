b0e483e3787652a8c172112d08658f6ebd684489
maxdml <maxdml@dbos.dev>
2025-04-07 18:18:08 -0700

    database_url

--- python/dbos-django-starter/polls/apps.py
+++ python/dbos-django-starter/polls/apps.py
@@ -1,3 +1,4 @@
+import os
 from django.apps import AppConfig
 from dbos import DBOS
 
@@ -8,6 +9,7 @@ class PollsConfig(AppConfig):
     def ready(self):
         dbos_config = {
             "name": "django-app",
+            "database_url": os.environ.get("DBOS_DATABASE_URL"),
         }
         dbos = DBOS(config=dbos_config)
         dbos.launch()
