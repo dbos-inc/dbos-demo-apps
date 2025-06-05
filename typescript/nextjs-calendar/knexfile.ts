207be6a41cf42a64d70822ac7d6a6602e12f1730
maxdml <maxdml@dbos.dev>
2025-06-04 15:50:28 -1000

    fix

--- typescript/nextjs-calendar/knexfile.ts
+++ typescript/nextjs-calendar/knexfile.ts
@@ -1,6 +1,6 @@
 const config = {
   client: 'pg',
-  connection: process.env.DBOS_DATABASE_URL || 'postgresql://postgres:dbos@localhost:5432/nextjs_calendar',
+  connection: process.env.DBOS_DATABASE_URL || 'postgresql://postgres:dbos@localhost:5432/dbos_next_calendar',
   migrations: {
     directory: './migrations'
   }
