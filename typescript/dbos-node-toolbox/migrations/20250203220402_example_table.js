e78d9e8ccf0b160c94730b3e64c36e75a570df52
Chuck B <chuck.bear@dbos.dev>
2025-06-25 10:53:05 -0400

    Update to v3 DS

--- typescript/dbos-node-toolbox/migrations/20250203220402_example_table.js
+++ typescript/dbos-node-toolbox/migrations/20250203220402_example_table.js
@@ -1,10 +1,18 @@
-exports.up = function(knex) {
-    return knex.schema.createTable('example_table', function(table) {
-      table.increments('count').primary();
-      table.string('name').notNullable();
-    });
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTable('example_table');
-  };
+const {
+  KnexDataSource
+} = require('@dbos-inc/knex-datasource');
+
+exports.up = async function(knex) {
+  await knex.schema.createTable('example_table', function(table) {
+    table.increments('count').primary();
+    table.string('name').notNullable();
+  });
+
+  await KnexDataSource.initializeSchema(knex);
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTable('example_table');
+
+  await KnexDataSource.uninitializeSchema(knex);
+};
