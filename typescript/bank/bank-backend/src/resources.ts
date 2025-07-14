4c4dd28e8a9dcc0f5ff849aa892f0ac0fbf4f0bd
Chuck B <chuck.bear@dbos.dev>
2025-07-14 15:23:45 -0400

    Migration

--- typescript/bank/bank-backend/src/resources.ts
+++ typescript/bank/bank-backend/src/resources.ts
@@ -7,7 +7,7 @@ export const dkoa = new DBOSKoa();
 const config = { user: process.env.PGUSER || 'postgres', database: 'bank_backend' };
 
 process.env['DATABASE_URL'] =
-  process.env['DATABSE_URL'] ||
+  process.env['DATABASE_URL'] ||
   `postgresql://${config.user}:${process.env['PGPASSWORD'] || 'dbos'}@${process.env['PGHOST'] || 'localhost'}:${process.env['PGPORT'] || '5432'}/${config.database}`;
 
 export const prismaClient = new PrismaClient();
