01393599a2b6e68a564b19eaf308d50e88ff359e
Peter Kraft <petereliaskraft@gmail.com>
2025-07-22 10:33:44 -0700

    update

--- golang/widget-store/main.go
+++ golang/widget-store/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"os"
-
 	"github.com/dbos-inc/dbos-transact-go/dbos"
 	"github.com/gin-gonic/gin"
 )
@@ -10,12 +8,7 @@ import (
 func main() {
 	r := gin.Default()
 
-	dbURL := os.Getenv("DBOS_DATABASE_URL")
-	if dbURL == "" {
-		panic("DBOS_DATABASE_URL environment variable is required")
-	}
-
-	err := dbos.Launch(dbos.WithDatabaseURL(dbURL))
+	err := dbos.Launch()
 	if err != nil {
 		panic(err)
 	}
