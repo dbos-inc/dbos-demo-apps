aa496191eb2112af828a9f6a61074d527bfbbd5e
chuck-dbos <134347445+chuck-dbos@users.noreply.github.com>
2025-07-28 21:49:38 -0400

    v3 Touchups (#330)
    Use DS instance instead of class for client; other small touchups

--- typescript/alert-center/src/utilities.ts
+++ typescript/alert-center/src/utilities.ts
@@ -45,7 +45,7 @@ const timeToRespondToAlert = 30;
 export class RespondUtilities {
   @knexds.transaction({readOnly: true})
   static async getAlertStatus() {
-    const alerts = await KnexDataSource.client<AlertEmployee>('alert_employee').select().orderBy(['alert_id']);
+    const alerts = await knexds.client<AlertEmployee>('alert_employee').select().orderBy(['alert_id']);
     for (const a of alerts) {
       if (a.alert_status === AlertStatus.ACTIVE && a.employee_name) {
         a.alert_status = AlertStatus.ASSIGNED;
@@ -57,13 +57,13 @@ export class RespondUtilities {
 
   @knexds.transaction()
   static async cleanAlerts() {
-    await KnexDataSource.client<AlertEmployee>('alert_employee').where({alert_status: AlertStatus.RESOLVED}).delete();
+    await knexds.client<AlertEmployee>('alert_employee').where({alert_status: AlertStatus.RESOLVED}).delete();
   }
 
 
   @knexds.transaction()
   static async getMaxId() {
-    const result = await KnexDataSource.client<AlertEmployee>('alert_employee').max('alert_id', { as: 'mid' }).first();
+    const result = await knexds.client<AlertEmployee>('alert_employee').max('alert_id', { as: 'mid' }).first();
     if (result ) {
       return result.mid;
     }
@@ -73,7 +73,7 @@ export class RespondUtilities {
 
   @knexds.transaction()
   static async addAlert(message: AlertWithMessage) {
-    await KnexDataSource.client<AlertEmployee>('alert_employee').insert({
+    await knexds.client<AlertEmployee>('alert_employee').insert({
       alert_id: message.alert_id,
       alert_status: message.alert_status,
       message: message.message,
@@ -84,27 +84,27 @@ export class RespondUtilities {
 
   @knexds.transaction()
   static async getUserAssignment(employee_name: string, currentTime: number, more_time: boolean | undefined) {
-    let employees = await KnexDataSource.client<Employee>('employee').where({employee_name}).select();
+    let employees = await knexds.client<Employee>('employee').where({employee_name}).select();
     let newAssignment = false;
 
     if (employees.length === 0) {
       //Is this the first getUserAssignment for this employee? Add them to the employee table
-      employees = await KnexDataSource.client<Employee>('employee').insert({employee_name, alert_id: null, expiration: null}).returning('*');
+      employees = await knexds.client<Employee>('employee').insert({employee_name, alert_id: null, expiration: null}).returning('*');
     }
 
     const expirationTime = new Date(currentTime + timeToRespondToAlert * 1000);
 
     if (!employees[0].alert_id) { 
       //This employee does not have a current assignment. Let's find a new one!
-      const op = await KnexDataSource.client<AlertEmployee>('alert_employee').whereNull('employee_name').orderBy(['alert_id']).first();
+      const op = await knexds.client<AlertEmployee>('alert_employee').whereNull('employee_name').orderBy(['alert_id']).first();
 
       if (op) { //found an alert - assign it
         op.employee_name = employee_name;
         const alert_id = op.alert_id;
         employees[0].alert_id = op.alert_id;
         employees[0].expiration = expirationTime;
-        await KnexDataSource.client<Employee>('employee').where({employee_name}).update({alert_id, expiration: expirationTime});
-        await KnexDataSource.client<AlertEmployee>('alert_employee').where({alert_id}).update({employee_name});
+        await knexds.client<Employee>('employee').where({employee_name}).update({alert_id, expiration: expirationTime});
+        await knexds.client<AlertEmployee>('alert_employee').where({alert_id}).update({employee_name});
         newAssignment = true;
         DBOS.logger.info(`New Assignment for ${employee_name}: ${alert_id}`);
       }
@@ -113,13 +113,13 @@ export class RespondUtilities {
       //This employee has an assignment and is asking for more time.
       DBOS.logger.info(`Extending time for ${employee_name} on ${employees[0].alert_id}`);
       employees[0].expiration = expirationTime;
-      await KnexDataSource.client<Employee>('employee').where({employee_name}).update({expiration: expirationTime});
+      await knexds.client<Employee>('employee').where({employee_name}).update({expiration: expirationTime});
     }
 
     //If we have an assignment (new or existing), retrieve and return it
     let alert : AlertEmployee[] = [];
     if (employees[0].alert_id) {
-      alert = await KnexDataSource.client<AlertEmployee>('alert_employee').where({alert_id: employees[0].alert_id}).select();
+      alert = await knexds.client<AlertEmployee>('alert_employee').where({alert_id: employees[0].alert_id}).select();
     }
     return {employee: employees[0], newAssignment, alert};
   }
@@ -127,20 +127,20 @@ export class RespondUtilities {
 
   @knexds.transaction()
   static async employeeCompleteAssignment(employee_name: string) {
-    const employees = await KnexDataSource.client<Employee>('employee').where({employee_name}).select();
+    const employees = await knexds.client<Employee>('employee').where({employee_name}).select();
     
     if (!employees[0].alert_id) {
       throw new Error(`Employee ${employee_name} completed an assignment that did not exist`);
     }
 
-    await KnexDataSource.client<AlertEmployee>('alert_employee').where({alert_id: employees[0].alert_id}).update({alert_status: AlertStatus.RESOLVED});
-    await KnexDataSource.client<Employee>('employee').where({employee_name}).update({alert_id: null, expiration: null});
+    await knexds.client<AlertEmployee>('alert_employee').where({alert_id: employees[0].alert_id}).update({alert_status: AlertStatus.RESOLVED});
+    await knexds.client<Employee>('employee').where({employee_name}).update({alert_id: null, expiration: null});
   }
 
 
   @knexds.transaction()
   static async employeeAbandonAssignment(employee_name: string) {
-    const employees = await KnexDataSource.client<Employee>('employee').where({employee_name}).select();
+    const employees = await knexds.client<Employee>('employee').where({employee_name}).select();
 
     if (!employees[0].alert_id) {
       // This employee is not assigned; nothing to abandon
@@ -148,15 +148,15 @@ export class RespondUtilities {
     }
 
     //Free up the alert for other employees to take
-    await KnexDataSource.client<AlertEmployee>('alert_employee').where({alert_id: employees[0].alert_id}).update({employee_name: null});
-    await KnexDataSource.client<Employee>('employee').where({employee_name}).update({alert_id: null, expiration: null});
+    await knexds.client<AlertEmployee>('alert_employee').where({alert_id: employees[0].alert_id}).update({employee_name: null});
+    await knexds.client<Employee>('employee').where({employee_name}).update({alert_id: null, expiration: null});
   }
 
 
   // This will return null if the assignment expired, or the expiration if an unexpired assignment exists
   @knexds.transaction()
   static async checkForExpiredAssignment(employee_name: string, currentTime: number) : Promise<Date | null> {
-    const employees = await KnexDataSource.client<Employee>('employee').where({employee_name}).select();
+    const employees = await knexds.client<Employee>('employee').where({employee_name}).select();
 
     if (!employees[0].alert_id) {
       // This employee is not assigned
@@ -170,8 +170,8 @@ export class RespondUtilities {
     }
 
     //This assigment expired - free up the alert for other employees to take
-    await KnexDataSource.client<AlertEmployee>('alert_employee').where({alert_id: employees[0].alert_id}).update({employee_name: null});
-    await KnexDataSource.client<Employee>('employee').where({employee_name}).update({alert_id: null, expiration: null});
+    await knexds.client<AlertEmployee>('alert_employee').where({alert_id: employees[0].alert_id}).update({employee_name: null});
+    await knexds.client<Employee>('employee').where({employee_name}).update({alert_id: null, expiration: null});
     return null;
   }
 }
