b13958a8e53b152bb5ad19b93dd7c5f73b5aed9d
Harry Pierson <harry.pierson@dbos.dev>
2025-08-26 15:46:17 -0700

    scaffolding

---
+++ java/dbos-starter/src/main/java/com/example/DbosStarter/DbosStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.DbosStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbosStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbosStarterApplication.class, args);
+	}
+
+}
---
+++ java/dbos-starter/src/test/java/com/example/DbosStarter/DbosStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DbosStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbosStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
