aad8320a4d41324acd03391ae43677dfd1e5defb
Harry Pierson <harry.pierson@dbos.dev>
2025-08-26 16:08:29 -0700

    add starter svc + impl

---
+++ java/dbos-starter/src/main/java/com/example/DbosStarter/service/DbosStarterService.java
@@ -0,0 +1,13 @@
+package com.example.DbosStarter.service;
+
+public interface DbosStarterService {
+
+    void setDbosStarterService(DbosStarterService service);
+
+    void exampleWorkflow();
+
+    void stepOne();
+    void stepTwo();
+    void stepThree();
+
+}
---
+++ java/dbos-starter/src/main/java/com/example/DbosStarter/service/DbosStarterServiceImpl.java
@@ -0,0 +1,65 @@
+package com.example.DbosStarter.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.dbos.transact.context.DBOSContext;
+import dev.dbos.transact.workflow.Step;
+import dev.dbos.transact.workflow.Workflow;
+
+public class DbosStarterServiceImpl implements DbosStarterService {
+
+    private DbosStarterService self;
+    private static final String stepsEvent = "steps_event";
+    private static final Logger logger = LoggerFactory.getLogger(DbosStarterServiceImpl.class);
+
+    @Override
+    public void setDbosStarterService(DbosStarterService self) {
+        this.self = self;
+    }
+
+    @Workflow
+    public void exampleWorkflow() {
+        var dbos = DBOSContext.dbosInstance().get();
+
+        self.stepOne();
+        dbos.setEvent(stepsEvent, 1);
+        self.stepTwo();
+        dbos.setEvent(stepsEvent, 2);
+        self.stepThree();
+        dbos.setEvent(stepsEvent, 3);
+    }
+
+    @Step
+    public void stepOne() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); 
+            logger.error("stepOne interrupted", e);
+        }
+        logger.info("Completed step 1!");
+    }
+
+    @Step
+    public void stepTwo() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); 
+            logger.error("stepTwo interrupted", e);
+        }
+        logger.info("Completed step 2!");
+    }
+
+    @Step
+    public void stepThree() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); 
+            logger.error("stepThree interrupted", e);
+        }
+        logger.info("Completed step 3!");
+    }
+}
