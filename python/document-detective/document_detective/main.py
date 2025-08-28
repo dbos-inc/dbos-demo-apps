9a66fdfc34ab8166fd1d883707bc9987fcfb4952
Peter Kraft <petereliaskraft@gmail.com>
2025-08-27 18:40:16 -0700

    update detective

--- python/document-detective/document_detective/main.py
+++ python/document-detective/document_detective/main.py
@@ -19,10 +19,14 @@ from sqlalchemy import make_url
 
 from .schema import chat_history
 
+application_database_url = os.environ.get('DBOS_DATABASE_URL')
+if not application_database_url:
+    raise Exception("DBOS_DATABASE_URL not set")
+
 app = FastAPI()
 config: DBOSConfig = {
     "name": "document-detective",
-    "database_url": os.environ.get("DBOS_DATABASE_URL"),
+    "application_database_url": application_database_url,
 }
 DBOS(fastapi=app, config=config)
 
@@ -32,7 +36,7 @@ DBOS(fastapi=app, config=config)
 
 def configure_index():
     Settings.chunk_size = 512
-    db = make_url(os.environ.get("DBOS_DATABASE_URL", "postgresql+psycopg://postgres:dbos@localhost:5432/document_detective"))
+    db = make_url(application_database_url)
     vector_store = PGVectorStore.from_params(
         database=db.database,
         host=db.host,
