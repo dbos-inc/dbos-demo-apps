661f8a05dd9b331aa08725dfe1079913369f2830
Harry Pierson <harry.pierson@dbos.dev>
2025-08-29 10:56:52 -0700

    update widget store to match dbos starter

---
+++ java/widget-store/src/main/java/com/example/widgetstore/config/DBOSLifecycle.java
@@ -0,0 +1,29 @@
+package com.example.widgetstore.config;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import dev.dbos.transact.DBOS;
+
+@Component
+public class DBOSLifecycle implements ApplicationListener<ApplicationEvent>  {
+
+    private final DBOS dbos;
+
+    public DBOSLifecycle(DBOS dbos) {
+        this.dbos = dbos;
+    }
+
+    @Override
+    public void onApplicationEvent(@NonNull ApplicationEvent event) {
+        if (event instanceof ApplicationReadyEvent) {
+            dbos.launch();
+        } else if (event instanceof ContextClosedEvent) {
+            dbos.shutdown(); // cleanup resources
+        }
+    }
+}
