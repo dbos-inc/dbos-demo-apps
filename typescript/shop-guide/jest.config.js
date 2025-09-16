34ff5630a42a9e7958f42cb95f0d95c721d77d8c
Peter Kraft <petereliaskraft@gmail.com>
2025-09-16 10:05:00 -0700

    remove

--- typescript/shop-guide/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: '((\\.|/)(test|spec))\\.ts?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  modulePaths: ["./"],
-  testTimeout: 1000 * 60 * 5,
-};
--- typescript/shop-guide/knexfile.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  client: 'pg',
-  connection: process.env.DBOS_DATABASE_URL || `postgresql://${process.env.PGUSER || 'postgres'}:${process.env.PGPASSWORD || 'dbos'}@${process.env.PGHOST || 'localhost'}:${process.env.PGPORT || '5432'}/${process.env.PGDATABASE || 'shop_guide'}`,
-  migrations: {
-    directory: './migrations'
-  }
-};
-
-module.exports = config;
