0e02cea6eeb9e2a644784579ca82ebba7c4a5f66
Peter Kraft <peter.kraft@dbos.dev>
2025-10-24 09:31:28 -0700

    Update Document Ingestion Demo (#365)

--- python/document-detective/README.md
+++ python/document-detective/README.md
@@ -1,12 +1,11 @@
 # Document Detective
 
-In this app, we use DBOS and LlamaIndex to build and serverlessly deploy a chatbot agent that can index PDF documents and answer questions about them.
+In this example, we'll use DBOS to build a reliable and scalable data processing pipeline. We'll show how DBOS can help you horizontally scale an application to process many items concurrently and seamlessly recover from failures. Specifically, we'll build a pipeline that indexes PDF documents for RAG, though you can use a similar design pattern to build almost any data pipeline.
 
 ## Creating an OpenAI Account
 
 To run this app, you need an OpenAI developer account.
 Obtain an API key [here](https://platform.openai.com/api-keys) and set up a payment method for your account [here](https://platform.openai.com/account/billing/overview).
-This bot uses `gpt-3.5-turbo` for text generation.
 Make sure you have some credits (~$1) to use it.
 
 Set your API key as an environment variable:
@@ -17,37 +16,36 @@ export OPENAI_API_KEY=<your_openai_key>
 
 ## Setup
 
-1. Install dependencies and activate your virtual environment:
+1. Install dependencies:
 
 ```shell
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+uv sync
 ```
 
 2. Start Postgres in a local Docker container:
 
 ```bash
-dbos postgres start
+uv run dbos postgres start
 ```
 
-Set the `DBOS_DATABASE_URL` environment variable to connect to this database:
+Set the `DBOS_SYSTEM_DATABASE_URL` environment variable to connect to this database:
 
 ```shell
-export DBOS_DATABASE_URL="postgresql+psycopg://postgres:dbos@localhost:5432/document_detective"
+export DBOS_SYSTEM_DATABASE_URL="postgresql+psycopg://postgres:dbos@localhost:5432/document_detective"
 ```
 
-If you already use Postgres, you can set the `DBOS_DATABASE_URL` environment variable to your own connection string.
-3. Run database migrations:
+If you already use Postgres, you can set the `DBOS_SYSTEM_DATABASE_URL` environment variable to your own connection string.
+
+3. Set up the Postgres vector store for LlamaIndex (requires pgvector):
 
 ```shell
-dbos migrate
+uv run python3 setup_llamaindex.py
 ```
 
 4. Start your app!
 
 ```shell
-python3 -m document_detective.main
+uv run python3 -m document_detective.main
 ```
 
 Visit [`http://localhost:8000`](http://localhost:8000) to see your chat agent!
@@ -57,12 +55,16 @@ Visit [`http://localhost:8000`](http://localhost:8000) to see your chat agent!
 
 To index a batch of PDF documents, send a list of their URLs in a POST request to the `/index` endpoint.
 
-For example, try this cURL command to index Apple's SEC 10-K filings for 2021, 2022, and 2023
-The application URL you should use is `http://localhost:8000` locally and your app URL in DBOS Cloud:
+For example, try this cURL command to index Apple's SEC 10-K filings for 2020-2024:
 
 ```shell
-curl -X POST "http://<URL>/index" \
+curl -X POST "http://localhost:8000/index" \
      -H "Content-Type: application/json" \
-     -d '{"urls": ["https://d18rn0p25nwr6d.cloudfront.net/CIK-0000320193/faab4555-c69b-438a-aaf7-e09305f87ca3.pdf", "https://d18rn0p25nwr6d.cloudfront.net/CIK-0000320193/b4266e40-1de6-4a34-9dfb-8632b8bd57e0.pdf", "https://d18rn0p25nwr6d.cloudfront.net/CIK-0000320193/42ede86f-6518-450f-bc88-60211bf39c6d.pdf"]}'
-
+     -d '{"urls": [
+        "https://dbos-hackathon.s3.us-east-1.amazonaws.com/apple-filings/apple-10k-2020.pdf",
+        "https://dbos-hackathon.s3.us-east-1.amazonaws.com/apple-filings/apple-10k-2021.pdf",
+        "https://dbos-hackathon.s3.us-east-1.amazonaws.com/apple-filings/apple-10k-2022.pdf",
+        "https://dbos-hackathon.s3.us-east-1.amazonaws.com/apple-filings/apple-10k-2023.pdf",
+        "https://dbos-hackathon.s3.us-east-1.amazonaws.com/apple-filings/apple-10k-2024.pdf"
+]}'
 ```
--- python/document-detective/document_detective/main.py
+++ python/document-detective/document_detective/main.py
@@ -1,7 +1,3 @@
-# In this app, we use DBOS and LlamaIndex to build and serverlessly deploy a chat agent that can index PDF documents and answer questions about them.
-
-# First, let's do imports and initialize DBOS.
-
 import os
 from tempfile import TemporaryDirectory
 from typing import List
@@ -11,32 +7,31 @@ import uvicorn
 from dbos import DBOS, DBOSConfig, Queue, WorkflowHandle
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from llama_index.core import Settings, StorageContext, VectorStoreIndex
+from llama_index.core import Document, Settings, StorageContext, VectorStoreIndex
 from llama_index.readers.file import PDFReader
 from llama_index.vector_stores.postgres import PGVectorStore
 from pydantic import BaseModel, HttpUrl
 from sqlalchemy import make_url
 
-from .schema import chat_history
-
-application_database_url = os.environ.get('DBOS_DATABASE_URL')
-if not application_database_url:
-    raise Exception("DBOS_DATABASE_URL not set")
+database_url = os.environ.get("DBOS_SYSTEM_DATABASE_URL")
+if not database_url:
+    raise Exception("DBOS_SYSTEM_DATABASE_URL not set")
 
 app = FastAPI()
 config: DBOSConfig = {
     "name": "document-detective",
-    "application_database_url": application_database_url,
+    "system_database_url": database_url,
+    "conductor_key": os.environ.get("CONDUCTOR_KEY"),
 }
-DBOS(fastapi=app, config=config)
+DBOS(config=config)
 
 
-# Next, let's initialize LlamaIndex to use Postgres with pgvector as its vector store.
+# First, let's initialize LlamaIndex to use Postgres with pgvector as its vector store.
 
 
 def configure_index():
     Settings.chunk_size = 512
-    db = make_url(application_database_url)
+    db = make_url(database_url)
     vector_store = PGVectorStore.from_params(
         database=db.database,
         host=db.host,
@@ -61,63 +56,84 @@ index, chat_engine = configure_index()
 # recover from where it left off instead of restarting from the beginning or losing
 # some documents entirely.
 
-# We'll build a concurrent, reliable data ingestion pipeline using DBOS queues and
-# durable execution. This workflow takes in a batch of document URLs and enqueues
-# them for ingestion. It then waits for them all to complete and counts how
-# many total documents and pages were ingested. If it's ever interrupted or restarted,
-# it recovers the ingestion of each document from the last completed step, guaranteeing
-# that every document gets ingested and none are lost.
+# We'll build a concurrent, reliable data ingestion pipeline using DBOS workflows and queues.
+# This workflow takes in a batch of document URLs and enqueues
+# them for indexing. It then waits for them all to complete and counts how
+# many total documents and pages were indexed. If it's ever interrupted or restarted,
+# it recovers the indexing of each document from the last completed step, guaranteeing
+# that every document gets indexed and none are lost.
 
 queue = Queue("indexing_queue")
 
 
 @DBOS.workflow()
-def indexing_workflow(urls: List[HttpUrl]):
+def index_documents(urls: List[HttpUrl]):
     handles: List[WorkflowHandle] = []
+    # Enqueue each document for indexing
     for url in urls:
         handle = queue.enqueue(index_document, url)
         handles.append(handle)
+    # Wait for all documents to finish indexing, count the total number of indexed pages
     indexed_pages = 0
     for handle in handles:
         indexed_pages += handle.get_result()
-    DBOS.logger.info(f"Indexed {len(urls)} documents totaling {indexed_pages} pages")
+    print(f"Indexed {len(urls)} documents totaling {indexed_pages} pages")
 
 
-# This function ingests a PDF document from a URL. It downloads it, scans it into pages,
-# then uses LlamaIndex to embed it and store the embedding in Postgres.
-
-# We annotate this function with DBOS.step() to mark it as a step in our indexing workflow.
-# Additionally, in case of transient failures (for example in downloading the document), we set it
-# to automatically retry indexing up to 5 times with exponential backoff.
+# This workflow indexes a document from a URL.
+# First, it downloads the document and parses it into pages.
+# Then, it adds each page to the vector index.
 
 
-@DBOS.step(retries_allowed=True, max_attempts=5)
+@DBOS.workflow()
 def index_document(document_url: HttpUrl) -> int:
+    pages = download_document(document_url)
+    for page in pages:
+        index_page(page)
+    return len(pages)
+
+
+@DBOS.step()
+def download_document(document_url: HttpUrl):
+    # Download the document to a temporary file
+    print(f"Downloading document from {document_url}")
     with TemporaryDirectory() as temp_dir:
-        temp_file_path = os.path.join(temp_dir, "file.pdf")
+        temp_file_path = os.path.join(temp_dir, "document.pdf")
         with open(temp_file_path, "wb") as temp_file:
             with requests.get(document_url, stream=True) as r:
                 r.raise_for_status()
                 for page in r.iter_content(chunk_size=8192):
                     temp_file.write(page)
-            temp_file.seek(0)
-            reader = PDFReader()
-            pages = reader.load_data(temp_file_path)
-    for page in pages:
+        # Parse the document into pages
+        reader = PDFReader()
+        pages = reader.load_data(temp_file_path)
+    return pages
+
+
+@DBOS.step()
+def index_page(page: Document):
+    # Insert a page into the vector index
+    try:
         index.insert(page)
-    return len(pages)
+    except Exception as e:
+        print("Error indexing page:", page, e)
 
 
 # This is the endpoint for indexing. It starts the indexing workflow in the background
 # on a batch of documents.
 
-# To test it out, try this example cURL command to index Apple's SEC 10-K filings
-# for 2021, 2022, and 2023.
+# For example, try this cURL command to index Apple's SEC 10-K filings for 2020-2024:
 
 """
 curl -X POST "http://localhost:8000/index" \
      -H "Content-Type: application/json" \
-     -d '{"urls": ["https://d18rn0p25nwr6d.cloudfront.net/CIK-0000320193/faab4555-c69b-438a-aaf7-e09305f87ca3.pdf", "https://d18rn0p25nwr6d.cloudfront.net/CIK-0000320193/b4266e40-1de6-4a34-9dfb-8632b8bd57e0.pdf", "https://d18rn0p25nwr6d.cloudfront.net/CIK-0000320193/42ede86f-6518-450f-bc88-60211bf39c6d.pdf"]}'
+     -d '{"urls": [
+        "https://dbos-hackathon.s3.us-east-1.amazonaws.com/apple-filings/apple-10k-2020.pdf",
+        "https://dbos-hackathon.s3.us-east-1.amazonaws.com/apple-filings/apple-10k-2021.pdf",
+        "https://dbos-hackathon.s3.us-east-1.amazonaws.com/apple-filings/apple-10k-2022.pdf",
+        "https://dbos-hackathon.s3.us-east-1.amazonaws.com/apple-filings/apple-10k-2023.pdf",
+        "https://dbos-hackathon.s3.us-east-1.amazonaws.com/apple-filings/apple-10k-2024.pdf"
+]}'
 """
 
 
@@ -126,65 +142,42 @@ class URLList(BaseModel):
 
 
 @app.post("/index")
-async def index_endpoint(urls: URLList):
-    DBOS.start_workflow(indexing_workflow, urls.urls)
+def index_endpoint(urls: URLList):
+    DBOS.start_workflow(index_documents, urls.urls)
 
 
-# Now, let's chat!
-
-# Each time we get a chat message, we call this workflow with three steps:
-# 1. Store the incoming chat message in Postgres.
-# 2. Query LlamaIndex to respond to the message using RAG.
-# 3. Store the response in Postgres.
+# Now, let's chat! We'll build a simple chat interface that stores history in memory.
+# Every time it gets a question, it answers using a RAG chat engine powered by the vector store.
 
 
 class ChatSchema(BaseModel):
     message: str
 
 
+chat_history = []
+
+
 @app.post("/chat")
-@DBOS.workflow()
-def chat_workflow(chat: ChatSchema):
-    insert_chat(chat.message, True)
-    response = query_model(chat.message)
-    insert_chat(response, False)
-    return {"content": response, "isUser": True}
+def chat(chat: ChatSchema):
+    query = {"content": chat.message, "isUser": False}
+    chat_history.append(query)
+    responseMessage = str(chat_engine.chat(chat.message))
+    response = {"content": responseMessage, "isUser": True}
+    chat_history.append(response)
+    return response
 
 
-@DBOS.transaction()
-def insert_chat(content: str, is_user: bool):
-    DBOS.sql_session.execute(
-        chat_history.insert().values(content=content, is_user=is_user)
-    )
-
-
-@DBOS.step()
-def query_model(message: str) -> str:
-    return str(chat_engine.chat(message))
-
-
-# Let's also write a history endpoint that retrieves all past chats
-# from the database.
-
+# Let's also write a history endpoint.
 # This function is called when we open up the chatbot so it
 # can display your chat history.
 
 
 @app.get("/history")
 def history_endpoint():
-    return get_chats()
-
-
-@DBOS.transaction()
-def get_chats():
-    stmt = chat_history.select().order_by(chat_history.c.created_at.asc())
-    result = DBOS.sql_session.execute(stmt)
-    return [{"content": row.content, "isUser": row.is_user} for row in result]
+    return chat_history
 
 
 # Finally, let's serve the app's frontend from an HTML file using FastAPI.
-# In production, we recommend using DBOS primarily for the backend,
-# with your frontend deployed elsewhere.
 
 
 @app.get("/")
@@ -192,6 +185,7 @@ def frontend():
     with open(os.path.join("html", "app.html")) as file:
         html = file.read()
     return HTMLResponse(html)
+
 
 if __name__ == "__main__":
     DBOS.launch()
