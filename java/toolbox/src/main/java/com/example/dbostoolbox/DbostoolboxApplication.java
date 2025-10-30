01a95be1f4549995ca22524df5f332698e8fabc4
Harry Pierson <harry.pierson@dbos.dev>
2025-10-30 10:42:42 -0700

    scaffold

---
+++ java/toolbox/src/main/java/com/example/dbostoolbox/DbostoolboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbostoolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbostoolboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbostoolboxApplication.class, args);
+	}
+
+}
---
+++ java/toolbox/src/test/java/com/example/dbostoolbox/DbostoolboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbostoolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbostoolboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
