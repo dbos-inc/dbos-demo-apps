8e165a5fc2923ddb8a89c056015cbc859890cf50
Peter Kraft <petereliaskraft@gmail.com>
2025-12-05 10:57:14 -0800

    delete

--- typescript/dbos-nextjs-starter/src/module-aliases.ts
+++
@@ -1,9 +0,0 @@
-import moduleAlias from "module-alias";
-
-// Map TypeScript paths (`src/dbos`) to runtime paths (`dist/dbos`)
-moduleAlias.addAliases({
-  "@dbos": __dirname + "/dbos",
-});
-
-// Apply the alias mapping
-moduleAlias();
--- typescript/e-commerce/npm-install.sh
+++
@@ -1,5 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-npm install --prefix $SCRIPT_DIR/shop-backend
-npm install --prefix $SCRIPT_DIR/shop-frontend
-npm install --prefix $SCRIPT_DIR/payment-backend
