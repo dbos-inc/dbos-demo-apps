5238763dbdb9bf0350ef7e0d659ac4969a71a120
Harry Pierson <harry.pierson@dbos.dev>
2026-01-08 13:08:18 -0800

    update dbos-starter to spring 4

--- java/dbos-starter/src/main/java/com/example/dbosstarter/config/DurableStarterConfig.java
+++ java/dbos-starter/src/main/java/com/example/dbosstarter/config/DurableStarterConfig.java
@@ -1,7 +1,5 @@
 package com.example.dbosstarter.config;
 
-import java.util.Objects;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
