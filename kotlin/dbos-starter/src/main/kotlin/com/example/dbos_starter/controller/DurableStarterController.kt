3abbce8a8f7904b0b006c494fac7c59ee194528b
Harry Pierson <harry.pierson@dbos.dev>
2026-01-08 17:15:46 -0800

    more cleanup

--- kotlin/dbos-starter/src/main/kotlin/com/example/dbos_starter/controller/DurableStarterController.kt
+++ kotlin/dbos-starter/src/main/kotlin/com/example/dbos_starter/controller/DurableStarterController.kt
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.time.Duration
-import kotlin.system.exitProcess
 
 private val logger = LoggerFactory.getLogger(DurableStarterController::class.java)
 
