1c547a955d9275a1681ed4819c689697a21d19a3
Peter Kraft <petereliaskraft@gmail.com>
2026-03-25 13:42:52 -0700

    simplify

--- typescript/dbos-nextjs-starter/worker/index.ts
+++ typescript/dbos-nextjs-starter/worker/index.ts
@@ -3,23 +3,10 @@ import { DBOS, WorkflowQueue } from "@dbos-inc/dbos-sdk";
 export const taskQueue = new WorkflowQueue("task_queue");
 
 async function greetingWorkflowFn(name: string) {
-  const greeting = await DBOS.runStep(
-    async () => {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      return `Hello, ${name}! The time is ${new Date().toLocaleTimeString()}.`;
-    },
+  return await DBOS.runStep(
+    async () => `Hello, ${name}!`,
     { name: "generateGreeting" }
   );
-
-  const uppercased = await DBOS.runStep(
-    async () => {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      return greeting.toUpperCase();
-    },
-    { name: "uppercaseGreeting" }
-  );
-
-  return uppercased;
 }
 
 DBOS.registerWorkflow(greetingWorkflowFn);
