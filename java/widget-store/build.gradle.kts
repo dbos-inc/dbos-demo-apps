69572f03f74bcf55aa75bea013f9067d0baa1837
Harry Pierson <harry.pierson@dbos.dev>
2026-07-02 08:48:49 -0700

    Version bump and starter style refresh (#446)
    Bump the JVM based demo apps to DBOS Transact 1.0.
    
    Also update the styling of the JVM DBOS starter apps to match #444

--- java/widget-store/build.gradle.kts
+++ java/widget-store/build.gradle.kts
@@ -16,7 +16,7 @@ java { toolchain { languageVersion = JavaLanguageVersion.of(17) } }
 repositories { mavenCentral() }
 
 dependencies {
-  val dbosVersion = "0.9.0"
+  val dbosVersion = "1.0.0"
   implementation("dev.dbos:transact-spring-boot-starter:$dbosVersion")
   implementation("dev.dbos:transact-spring-txstep-starter:$dbosVersion")
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
