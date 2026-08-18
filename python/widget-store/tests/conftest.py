215d4dc71bfe439935637f31b5aa734871a98aa3
Peter Kraft <petereliaskraft@gmail.com>
2026-08-18 14:39:51 -0700

    fix

--- python/widget-store/tests/conftest.py
+++ python/widget-store/tests/conftest.py
@@ -8,19 +8,12 @@ from sqlalchemy.engine.url import make_url
 import widget_store.main as widget_store
 
 
-def reset_database(test_database_url: str):
+def create_database(test_database_url: str):
     url = make_url(test_database_url)
-    database = url.database
     postgres_db_url = url.set(database="postgres")
     engine = sa.create_engine(postgres_db_url, isolation_level="AUTOCOMMIT")
     with engine.connect() as conn:
-        conn.execute(
-            sa.text(
-                f"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{database}'"
-            )
-        )
-        conn.execute(sa.text(f"DROP DATABASE IF EXISTS {database}"))
-        conn.execute(sa.text(f"CREATE DATABASE {database}"))
+        conn.execute(sa.text(f"CREATE DATABASE {url.database}"))
     engine.dispose()
 
 
@@ -40,7 +33,8 @@ def dbos(test_database_url):
         "system_database_url": test_database_url,
         "application_version": "0.1.0",
     }
-    reset_database(test_database_url)
     DBOS(config=config)
+    DBOS.reset_system_database()
+    create_database(test_database_url)
     widget_store.ds = SQLAlchemyDatasource.create(test_database_url)
     DBOS.launch()
