f5ff8ce7cbf930d2fa38781174fede5b83efac70
Harry Pierson <harry.pierson@dbos.dev>
2026-09-24 15:39:10 -0700

    JVM demo apps: DBOS Transact Java 1.1.0
    Move the Java, Kotlin, Scala, and Clojure apps from the 1.1.0-m43
    milestone to the 1.1.0 release.

--- kotlin/queue-patterns/app/build.gradle.kts
+++ kotlin/queue-patterns/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 repositories { mavenCentral() }
 
 dependencies {
-  implementation("dev.dbos:transact:1.1.0-m43")
+  implementation("dev.dbos:transact:1.1.0")
 
   implementation("io.javalin:javalin:7.2.2")
   implementation("org.slf4j:slf4j-simple:2.0.18")
